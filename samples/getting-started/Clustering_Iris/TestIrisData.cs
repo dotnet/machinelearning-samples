c2aa503bd653848a6efc13a72f3a8efa856f4edd
Bojan Misic <misic.bojan@gmail.com>
2018-06-30 22:19:52 +0200

    Fix iris dataset column order

--- samples/getting-started/Clustering_Iris/TestIrisData.cs
+++ samples/getting-started/Clustering_Iris/TestIrisData.cs
@@ -4,50 +4,50 @@
     {
         internal static readonly IrisData Setosa1 = new IrisData()
         {
-            SepalLength = 3.3f,
-            SepalWidth = 1.6f,
-            PetalLength = 0.2f,
-            PetalWidth = 5.1f,
+            SepalLength = 5.1f,
+            SepalWidth = 3.3f,
+            PetalLength = 1.6f,
+            PetalWidth = 0.2f,
         };
 
         internal static readonly IrisData Setosa2 = new IrisData()
         {
-            SepalLength = 5.1f,			
-            SepalWidth = 3.5f,
-            PetalLength = 1.4f,
-            PetalWidth = 0.2f,
+            SepalLength = 0.2f,			
+            SepalWidth = 5.1f,
+            PetalLength = 3.5f,
+            PetalWidth = 1.4f,
         };
 
         internal static readonly IrisData Virginica1 = new IrisData()
         {
-            SepalLength = 3.1f,
-            SepalWidth = 5.5f,
-            PetalLength = 2.2f,
-            PetalWidth = 6.4f,
+            SepalLength = 6.4f,
+            SepalWidth = 3.1f,
+            PetalLength = 5.5f,
+            PetalWidth = 2.2f,
         };
 
         internal static readonly IrisData Virginica2 = new IrisData()
         {
-            SepalLength = 6.3f, 			
-            SepalWidth = 3.3f,
-            PetalLength = 6f,
-            PetalWidth = 2.5f,
+            SepalLength = 2.5f, 			
+            SepalWidth = 6.3f,
+            PetalLength = 3.3f,
+            PetalWidth = 6f,
         };
 
         internal static readonly IrisData Versicolor1 = new IrisData()
         {
-            SepalLength = 3.1f,
-            SepalWidth = 4.5f,
-            PetalLength = 1.5f,
-            PetalWidth = 6.4f,
+            SepalLength = 6.4f,
+            SepalWidth = 3.1f,
+            PetalLength = 4.5f,
+            PetalWidth = 1.5f,
         };
 
         internal static readonly IrisData Versicolor2 = new IrisData()
         {
-            SepalLength = 3.2f, 			
-            SepalWidth = 4.7f,
-            PetalLength = 1.4f,
-            PetalWidth = 7.0f,
+            SepalLength = 7.0f, 			
+            SepalWidth = 3.2f,
+            PetalLength = 4.7f,
+            PetalWidth = 1.4f,
         };
     }
 }
--- samples/getting-started/MulticlassClassification_Iris/TestIrisData.cs
+++ samples/getting-started/MulticlassClassification_Iris/TestIrisData.cs
@@ -6,26 +6,26 @@
         {
             internal static readonly IrisData Iris1 = new IrisData()
             {
-                SepalLength = 3.3f,
-                SepalWidth = 1.6f,
-                PetalLength = 0.2f,
-                PetalWidth= 5.1f,
+                SepalLength = 5.1f,
+                SepalWidth = 3.3f,
+                PetalLength = 1.6f,
+                PetalWidth= 0.2f,
             };
 
             internal static readonly IrisData Iris2 = new IrisData()
             {
-                SepalLength = 3.1f,
-                SepalWidth = 5.5f,
-                PetalLength = 2.2f,
-                PetalWidth = 6.4f,
+                SepalLength = 6.4f,
+                SepalWidth = 3.1f,
+                PetalLength = 5.5f,
+                PetalWidth = 2.2f,
             };
 
             internal static readonly IrisData Iris3 = new IrisData()
             {
-                SepalLength = 3.1f,
-                SepalWidth = 2.5f,
-                PetalLength = 1.2f,
-                PetalWidth = 4.4f,
+                SepalLength = 4.4f,
+                SepalWidth = 3.1f,
+                PetalLength = 2.5f,
+                PetalWidth = 1.2f,
             };
         }
     }
