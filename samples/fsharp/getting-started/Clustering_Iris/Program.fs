a03ff0bf0710633ade85132be4a164b8235a2331
Don Syme <dsyme@microsoft.com>
2018-08-03 17:28:16 +0100

    add F# samples

---
+++ samples/fsharp/getting-started/Clustering_Iris/Program.fs
@@ -0,0 +1,94 @@
+﻿module Clustering_Iris
+
+open System
+open System.IO
+open Microsoft.ML
+open Microsoft.ML.Runtime.Api
+open Microsoft.ML.Data
+open Microsoft.ML.Trainers
+open Microsoft.ML.Transforms
+
+let AppPath = Path.Combine(__SOURCE_DIRECTORY__, "../../../..")
+let DataPath = Path.Combine(AppPath, "datasets", "iris-full.txt")
+let ModelPath = Path.Combine(AppPath, "IrisClustersModel.zip")
+
+type IrisData() = 
+    [<Column("0")>]
+    member val Label = 0.0 with get,set
+
+    [<Column("1")>]
+    member val SepalLength = 0.0 with get, set
+
+    [<Column("2")>]
+    member val SepalWidth = 0.0 with get, set
+
+    [<Column("3")>]
+    member val PetalLength = 0.0 with get, set
+
+    [<Column("4")>]
+    member val PetalWidth = 0.0 with get, set
+
+type ClusterPrediction() = 
+    [<ColumnName("PredictedLabel")>]
+    member val SelectedClusterId = 0 with get, set
+
+    [<ColumnName("Score")>]
+    member val  Distance : float[] = null with get, set
+
+let Train() =
+    // LearningPipeline holds all steps of the learning process: data, transforms, learners.
+    let pipeline = LearningPipeline()
+    // The TextLoader loads a dataset. The schema of the dataset is specified by passing a class containing
+    // all the column names and their types.
+    pipeline.Add(TextLoader(DataPath).CreateFrom<IrisData>(useHeader=true))
+    // ColumnConcatenator concatenates all columns into Features column
+    pipeline.Add(ColumnConcatenator("Features",
+                                    "SepalLength",
+                                    "SepalWidth",
+                                    "PetalLength",
+                                    "PetalWidth"))
+    // KMeansPlusPlusClusterer is an algorithm that will be used to build clusters. We set the number of clusters to 3.
+    pipeline.Add(KMeansPlusPlusClusterer(K = 3))
+
+    Console.WriteLine("=============== Training model ===============")
+    let model = pipeline.Train<IrisData, ClusterPrediction>()
+    Console.WriteLine("=============== End training ===============")
+    
+    // Saving the model as a .zip file.
+    model.WriteAsync(ModelPath) |> Async.AwaitTask |> Async.RunSynchronously
+    Console.WriteLine("The model is saved to {0}", ModelPath)
+   
+    model
+
+module TestIrisData = 
+    let Setosa1 = IrisData(SepalLength = 5.1, SepalWidth = 3.3, PetalLength = 1.6, PetalWidth = 0.2)
+    let Setosa2 = IrisData(SepalLength = 0.2, SepalWidth = 5.1, PetalLength = 3.5, PetalWidth = 1.4)
+    let Virginica1 = IrisData(SepalLength = 6.4, SepalWidth = 3.1, PetalLength = 5.5, PetalWidth = 2.2)
+    let Virginica2 = IrisData(SepalLength = 2.5, SepalWidth = 6.3, PetalLength = 3.3, PetalWidth = 6.0)
+    let Versicolor1 = IrisData(SepalLength = 6.4, SepalWidth = 3.1, PetalLength = 4.5, PetalWidth = 1.5)
+    let Versicolor2 = IrisData(SepalLength = 7.0, SepalWidth = 3.2, PetalLength = 4.7, PetalWidth = 1.4)
+
+// STEP 1: Create a model
+let model = Train()
+        
+// STEP 2: Make a prediction
+Console.WriteLine()
+let prediction1 = model.Predict(TestIrisData.Setosa1)
+let prediction2 = model.Predict(TestIrisData.Setosa2)          
+Console.WriteLine(sprintf "Clusters assigned for setosa flowers:")
+Console.WriteLine(sprintf "                                        {%d}" prediction1.SelectedClusterId)
+Console.WriteLine(sprintf "                                        {%d}" prediction2.SelectedClusterId)
+
+let prediction3 = model.Predict(TestIrisData.Virginica1)
+let prediction4 = model.Predict(TestIrisData.Virginica2)
+Console.WriteLine(sprintf "Clusters assigned for virginica flowers:")
+Console.WriteLine(sprintf "                                        {%d}" prediction3.SelectedClusterId)
+Console.WriteLine(sprintf "                                        {%d}" prediction4.SelectedClusterId)
+
+let prediction5 = model.Predict(TestIrisData.Versicolor1)
+let prediction6 = model.Predict(TestIrisData.Versicolor2)
+Console.WriteLine(sprintf "Clusters assigned for versicolor flowers:")
+Console.WriteLine(sprintf "                                        {%d}" prediction5.SelectedClusterId)
+Console.WriteLine(sprintf "                                        {%d}" prediction6.SelectedClusterId)
+Console.ReadLine() |> ignore
+
---
+++ samples/fsharp/getting-started/MulticlassClassification_Iris/Program.fs
@@ -0,0 +1,136 @@
+﻿module MulticlassClassification_Iris
+
+open System
+open System.IO
+open Microsoft.ML
+open Microsoft.ML.Data
+open Microsoft.ML.Runtime.Api
+open Microsoft.ML.Models
+open Microsoft.ML.Trainers
+open Microsoft.ML.Transforms
+
+let AppPath = Path.Combine(__SOURCE_DIRECTORY__, "../../../..")
+let TrainDataPath= Path.Combine(AppPath, "datasets", "iris-train.txt")
+let TestDataPath= Path.Combine(AppPath,  "datasets", "iris-test.txt")
+let ModelPath= Path.Combine(AppPath, "IrisModel.zip")
+
+type IrisData() = 
+    [<Column("0")>]
+    member val Label = 0.0 with get,set
+
+    [<Column("1")>]
+    member val SepalLength = 0.0 with get, set
+
+    [<Column("2")>]
+    member val SepalWidth = 0.0 with get, set
+
+    [<Column("3")>]
+    member val PetalLength = 0.0 with get, set
+
+    [<Column("4")>]
+    member val PetalWidth = 0.0 with get, set
+
+type IrisPrediction() = 
+
+    [<ColumnName("Score")>]
+    member val  Score : float[] = null with get, set
+
+
+let TrainAsync() =
+   // LearningPipeline holds all steps of the learning process: data, transforms, learners.
+    let pipeline = LearningPipeline()
+        // The TextLoader loads a dataset. The schema of the dataset is specified by passing a class containing
+        // all the column names and their types.
+    pipeline.Add(TextLoader(TrainDataPath).CreateFrom<IrisData>())
+        
+    // Transforms
+    // When ML model starts training, it looks for two columns: Label and Features.
+    // Label:   values that should be predicted. If you have a field named Label in your data type,
+    //              like in this example, no extra actions required.
+    //          If you don’t have it, copy the column you want to predict with ColumnCopier transform:
+    //              new ColumnCopier(("FareAmount", "Label"))
+    // Features: all data used for prediction. At the end of all transforms you need to concatenate
+    //              all columns except the one you want to predict into Features column with
+    //              ColumnConcatenator transform:
+    pipeline.Add(ColumnConcatenator("Features",
+                    "SepalLength",
+                    "SepalWidth",
+                    "PetalLength",
+                    "PetalWidth"))
+
+    // StochasticDualCoordinateAscentClassifier is an algorithm that will be used to train the model.
+    pipeline.Add(StochasticDualCoordinateAscentClassifier())
+
+    Console.WriteLine("=============== Training model ===============")
+    // The pipeline is trained on the dataset that has been loaded and transformed.
+    let model = pipeline.Train<IrisData, IrisPrediction>()
+
+    // Saving the model as a .zip file.
+    model.WriteAsync(ModelPath) |> Async.AwaitTask |> Async.RunSynchronously
+
+    Console.WriteLine("=============== End training ===============")
+    Console.WriteLine("The model is saved to {0}", ModelPath)
+
+    model
+
+module TestIrisData = 
+    let Iris1 = IrisData(SepalLength = 5.1, SepalWidth = 3.3, PetalLength = 1.6, PetalWidth= 0.2)
+    let Iris2 = IrisData(SepalLength = 6.4, SepalWidth = 3.1, PetalLength = 5.5, PetalWidth = 2.2)
+    let Iris3 = IrisData(SepalLength = 4.4, SepalWidth = 3.1, PetalLength = 2.5, PetalWidth = 1.2)
+
+let Evaluate(model : PredictionModel<IrisData, IrisPrediction>) =
+    // To evaluate how good the model predicts values, the model is ran against new set
+    // of data (test data) that was not involved in training.
+    let testData = TextLoader(TestDataPath).CreateFrom<IrisData>()
+    
+    // ClassificationEvaluator performs evaluation for Multiclass Classification type of ML problems.
+    let evaluator = ClassificationEvaluator(OutputTopKAcc = Nullable(3))
+    
+    Console.WriteLine("=============== Evaluating model ===============")
+
+    let metrics = evaluator.Evaluate(model, testData)
+    Console.WriteLine("Metrics:")
+    Console.WriteLine(sprintf "    AccuracyMacro = {metrics.AccuracyMacro:0.####}, a value between 0 and 1, the closer to 1, the better")
+    Console.WriteLine(sprintf "    AccuracyMicro = {metrics.AccuracyMicro:0.####}, a value between 0 and 1, the closer to 1, the better")
+    Console.WriteLine(sprintf "    LogLoss = {metrics.LogLoss:0.####}, the closer to 0, the better")
+    Console.WriteLine(sprintf "    LogLoss for class 1 = {metrics.PerClassLogLoss[0]:0.####}, the closer to 0, the better")
+    Console.WriteLine(sprintf "    LogLoss for class 2 = {metrics.PerClassLogLoss[1]:0.####}, the closer to 0, the better")
+    Console.WriteLine(sprintf "    LogLoss for class 3 = {metrics.PerClassLogLoss[2]:0.####}, the closer to 0, the better")
+    Console.WriteLine()
+    Console.WriteLine(sprintf "    ConfusionMatrix:")
+
+    // Print confusion matrix
+    for i in 0 .. metrics.ConfusionMatrix.Order - 1 do
+        for j in 0 .. metrics.ConfusionMatrix.ClassNames.Count - 1 do
+            Console.Write("\t" + string metrics.ConfusionMatrix.[i, j])
+        Console.WriteLine()
+
+    Console.WriteLine("=============== End evaluating ===============")
+    Console.WriteLine()
+
+// STEP 1: Create a model
+let model = TrainAsync()
+
+// STEP2: Test accuracy
+Evaluate(model)
+
+// STEP 3: Make a prediction
+Console.WriteLine()
+let prediction1 = model.Predict(TestIrisData.Iris1)
+Console.WriteLine(sprintf "Actual: setosa.     Predicted probability: setosa:      %0.4f" prediction1.Score.[0])
+Console.WriteLine(sprintf "                                           versicolor:  %0.4f" prediction1.Score.[1])
+Console.WriteLine(sprintf "                                           virginica:   %0.4f" prediction1.Score.[2])
+Console.WriteLine()
+
+let prediction2 = model.Predict(TestIrisData.Iris2)
+Console.WriteLine(sprintf "Actual: virginica.  Predicted probability: setosa:      %0.4f" prediction2.Score.[0])
+Console.WriteLine(sprintf "                                           versicolor:  %0.4f" prediction2.Score.[1])
+Console.WriteLine(sprintf "                                           virginica:   %0.4f" prediction2.Score.[2])
+Console.WriteLine()
+
+let prediction3 = model.Predict(TestIrisData.Iris3)
+Console.WriteLine(sprintf "Actual: versicolor. Predicted probability: setosa:      %0.4f" prediction3.Score.[0])
+Console.WriteLine(sprintf "                                           versicolor:  %0.4f" prediction3.Score.[1])
+Console.WriteLine(sprintf "                                           virginica:   %0.4f" prediction3.Score.[2])
+
+Console.ReadLine() |> ignore
