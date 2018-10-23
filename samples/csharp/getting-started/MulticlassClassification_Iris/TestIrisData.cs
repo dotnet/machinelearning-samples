6a939dd2d80d782220bf19def6902188bf4062a1
Mariusz Wojcik <mariusz.wojcik@hotmail.co.uk>
2018-10-23 06:25:58 +0100

    Add MulticlassClassification_Iris C# sample.

---
+++ samples/csharp/getting-started/MulticlassClassification_Iris/TestIrisData.cs
@@ -0,0 +1,40 @@
+﻿namespace MulticlassClassification_Iris
+{
+    public static partial class Program
+    {
+        internal class TestIrisData
+        {
+            internal static readonly IrisData Iris0 = new IrisData()
+            {
+                SepalLength = 5.0f,
+                SepalWidth = 2.0f,
+                PetalLength = 3.5f,
+                PetalWidth = 1.0f,
+            };
+
+            internal static readonly IrisData Iris1 = new IrisData()
+            {
+                SepalLength = 5.1f,
+                SepalWidth = 3.3f,
+                PetalLength = 1.6f,
+                PetalWidth= 0.2f,
+            };
+
+            internal static readonly IrisData Iris2 = new IrisData()
+            {
+                SepalLength = 6.4f,
+                SepalWidth = 3.1f,
+                PetalLength = 5.5f,
+                PetalWidth = 2.2f,
+            };
+
+            internal static readonly IrisData Iris3 = new IrisData()
+            {
+                SepalLength = 4.4f,
+                SepalWidth = 3.1f,
+                PetalLength = 2.5f,
+                PetalWidth = 1.2f,
+            };
+        }
+    }
+}
