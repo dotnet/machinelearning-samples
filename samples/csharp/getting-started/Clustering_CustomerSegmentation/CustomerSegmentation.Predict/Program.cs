de499c558d640b36334109b3f41b123a321354cf
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-10-24 19:03:08 -0700

    Baseline for Customer Segmentation (Clustering Sample)

---
+++ samples/csharp/getting-started/Clustering_CustomerSegmentation/CustomerSegmentation.Predict/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CustomerSegmentation.Model;
+using System.IO;
+using System.Threading.Tasks;
+using static CustomerSegmentation.Model.ConsoleHelpers;
+
+namespace CustomerSegmentation
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var assetsPath = ModelHelpers.GetAssetsPath(@"..\..\..\assets");
+
+            var pivotCsv = Path.Combine(assetsPath, "inputs", "pivot.csv");
+            var modelZip = Path.Combine(assetsPath, "inputs", "retailClustering.zip");
+            var plotSvg = Path.Combine(assetsPath, "outputs", "customerSegmentation.svg");
+
+            try
+            {
+                var modelEvaluator = new ModelEvaluator(pivotCsv, modelZip, plotSvg);
+                modelEvaluator.Evaluate();
+            } catch (Exception ex)
+            {
+                ConsoleWriteException(ex.Message);
+            }
+
+            ConsolePressAnyKey();
+        }
+    }
+}
---
+++ samples/csharp/getting-started/Clustering_CustomerSegmentation/CustomerSegmentation.Train/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CustomerSegmentation.Model;
+using System.IO;
+using System.Threading.Tasks;
+using CustomerSegmentation.RetailData;
+using static CustomerSegmentation.Model.ConsoleHelpers;
+
+namespace CustomerSegmentation
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var assetsPath = ModelHelpers.GetAssetsPath(@"..\..\..\assets");
+
+            var transactionsCsv = Path.Combine(assetsPath, "inputs", "transactions.csv");
+            var offersCsv = Path.Combine(assetsPath, "inputs", "offers.csv");
+            var pivotCsv = Path.Combine(assetsPath, "inputs", "pivot.csv");
+            var modelZip = Path.Combine(assetsPath, "outputs", "retailClustering.zip");
+            var kValuesSvg = Path.Combine(assetsPath, "outputs", "kValues.svg");
+
+            try
+            {
+                DataHelpers.PreProcessAndSave(offersCsv, transactionsCsv, pivotCsv);
+                var modelBuilder = new ModelBuilder(pivotCsv, modelZip, kValuesSvg);
+                modelBuilder.BuildAndTrain();
+            } catch (Exception ex)
+            {
+                ConsoleWriteException(ex.Message);
+            }
+
+            ConsolePressAnyKey();
+        }
+    }
+}
