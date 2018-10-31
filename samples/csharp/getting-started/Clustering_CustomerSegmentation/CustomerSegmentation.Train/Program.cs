6907e67cd01d884ffbaa3fae3b04a4838c4eea4e
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-10-31 13:31:14 -0700

    Refactoring CustomerSegmentation.Train project code

--- samples/csharp/getting-started/Clustering_CustomerSegmentation/CustomerSegmentation.Train/Program.cs
+++ samples/csharp/getting-started/Clustering_CustomerSegmentation/CustomerSegmentation.Train/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using CustomerSegmentation.RetailData;
 using static CustomerSegmentation.Model.ConsoleHelpers;
+using Microsoft.ML.Runtime.Data;
 
 namespace CustomerSegmentation
 {
@@ -17,13 +18,31 @@ namespace CustomerSegmentation
             var offersCsv = Path.Combine(assetsPath, "inputs", "offers.csv");
             var pivotCsv = Path.Combine(assetsPath, "inputs", "pivot.csv");
             var modelZip = Path.Combine(assetsPath, "outputs", "retailClustering.zip");
-            var kValuesSvg = Path.Combine(assetsPath, "outputs", "kValues.svg");
 
             try
             {
+                //DataHelpers.PreProcessAndSave(offersCsv, transactionsCsv, pivotCsv);
+                //var modelBuilder = new ModelBuilder(pivotCsv, modelZip, kValuesSvg);
+                //modelBuilder.BuildAndTrain();
+
                 DataHelpers.PreProcessAndSave(offersCsv, transactionsCsv, pivotCsv);
-                var modelBuilder = new ModelBuilder(pivotCsv, modelZip, kValuesSvg);
-                modelBuilder.BuildAndTrain();
+
+                //Create the MLContext to share across components for deterministic results
+                LocalEnvironment mlContext = new LocalEnvironment(seed: 1);  //Seed set to any number so you have a deterministic environment
+
+                // STEP 1: Create and train a model
+                var modelBuilder = new ModelBuilder(mlContext);
+                var model = modelBuilder.BuildAndTrain(pivotCsv);
+
+                // STEP2: Evaluate accuracy of the model
+                modelBuilder.Evaluate(pivotCsv, model);
+
+                // STEP3: Save model
+                modelBuilder.SaveModel(modelZip, model);
+
+                Console.WriteLine("Press any key to exit..");
+                Console.ReadLine();
+
             } catch (Exception ex)
             {
                 ConsoleWriteException(ex.Message);
