1cd48a8d4813bd9f193112039ea93503a373ae6d
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-11-01 18:45:19 -0700

    BikeSharingDemand with Common-code approach

--- samples/csharp/getting-started/Regression_BikeSharingDemand/BikeSharingDemand/BikeSharingDemandConsoleApp/Program.cs
+++ samples/csharp/getting-started/Regression_BikeSharingDemand/BikeSharingDemand/BikeSharingDemandConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using BikeSharingDemand.Helpers;
 using Microsoft.ML;
 using Microsoft.ML.Core.Data;
 using Microsoft.ML.Runtime;
@@ -11,9 +10,9 @@ namespace BikeSharingDemand
 {
     internal static class Program
     {
-        private static string ModelsLocation = @"../../../../models";
+        private static string ModelsLocation = @"../../../../MLModels";
 
-        private static string DatasetsLocation = @"../../../../data";
+        private static string DatasetsLocation = @"../../../../Data";
         private static string TrainingDataLocation = $"{DatasetsLocation}/hour_train.csv";
         private static string TestDataLocation = $"{DatasetsLocation}/hour_test.csv";
         
@@ -33,8 +32,8 @@ namespace BikeSharingDemand
             var dataPreprocessPipeline = dataPreprocessor.DataPreprocessPipeline;
 
             // (Optional) Peek data in training DataView after applying the PreprocessPipeline's transformations  
-            ConsoleHelper.PeekDataViewInConsole(mlContext, trainingDataView, dataPreprocessPipeline, 10);
-            ConsoleHelper.PeekFeaturesColumnDataInConsole(mlContext, "Features", trainingDataView, dataPreprocessPipeline, 10);
+            Common.ConsoleHelper.PeekDataViewInConsole<DemandObservation>(mlContext, trainingDataView, dataPreprocessPipeline, 10);
+            Common.ConsoleHelper.PeekFeaturesColumnDataInConsole(mlContext, "Features", trainingDataView, dataPreprocessPipeline, 10);
 
             // Definition of regression trainers/algorithms to use
             var regressionLearners = new (string name, IEstimator<ITransformer> value)[]
@@ -54,34 +53,36 @@ namespace BikeSharingDemand
             foreach (var learner in regressionLearners)
             {
                 Console.WriteLine("================== Training model ==================");
-                var modelBuilder = new ModelBuilder(mlContext, dataPreprocessPipeline, learner.value);
+                var modelBuilder = new Common.ModelBuilder<DemandObservation,DemandPrediction>(mlContext, dataPreprocessPipeline, learner.value);
                 var trainedModel = modelBuilder.Train(trainingDataView);
 
                 Console.WriteLine("========= Predict a single data point ===============");
                 var prediction = modelBuilder.PredictSingle(DemandObservationSample.SingleDemandSampleData);
-                ConsoleHelper.PrintPrediction(prediction);
+                Common.ConsoleHelper.PrintPrediction(prediction.PredictedCount.ToString());
 
                 Console.WriteLine("===== Evaluating Model's accuracy with Test data =====");
                 var metrics = modelBuilder.Evaluate(testDataView);
-                ConsoleHelper.PrintRegressionMetrics(learner.name, metrics);
+                Common.ConsoleHelper.PrintRegressionMetrics(learner.name, metrics);
 
                 //Save the model file that can be used by any application
                 modelBuilder.SaveAsFile($"{ModelsLocation}/{learner.name}Model.zip");
             }
 
-            // 4. Test/Predict with the created models
+            // 4. Try/test Predictions with the created models
             // The following test predictions could be implemented/deployed in a different application (production apps)
             // that's why it is seggregated from the previous loop
             // For each trained model, test 10 predictions           
             foreach (var learner in regressionLearners)
             {
                 //Load current model
-                var trainedModel = ModelTester.LoadModelFromZipFile(mlContext, $"{ModelsLocation}/{learner.name}Model.zip");
+                var trainedModel = Common.ModelScorer.LoadModelFromZipFile(mlContext, $"{ModelsLocation}/{learner.name}Model.zip");
                 Console.WriteLine($"================== Visualize/test 10 predictions for model {learner.name}Model.zip ==================");
                 //Visualize 10 tests comparing prediction with actual/observed values from the test dataset
-                ModelTester.VisualizeSomePredictions(learner.name, TestDataLocation, trainedModel, 10);
+                ModelScoringTester.VisualizeSomePredictions(learner.name, TestDataLocation, trainedModel, 10);
             }
-            Console.ReadLine();
+
+            Common.ConsoleHelper.ConsolePressAnyKey();
+
         }
     }
 }
