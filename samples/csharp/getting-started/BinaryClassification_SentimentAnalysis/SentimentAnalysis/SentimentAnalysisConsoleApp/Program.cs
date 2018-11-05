c05d60873e432ad9d21be0c866b9ea37e5c49708
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-11-05 11:55:14 -0800

    Changed to FastTree for SentimentAnalysis

--- samples/csharp/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/Program.cs
+++ samples/csharp/getting-started/BinaryClassification_SentimentAnalysis/SentimentAnalysis/SentimentAnalysisConsoleApp/Program.cs
@@ -44,7 +44,8 @@ namespace SentimentAnalysisConsoleApp
 
             // STEP 3: Set the training algorithm, then create and config the modelBuilder                            
             var modelBuilder = new Common.ModelBuilder<SentimentIssue, SentimentPrediction>(mlContext, dataProcessPipeline);
-            var trainer = mlContext.BinaryClassification.Trainers.StochasticDualCoordinateAscent(label: "Label", features: "Features");
+            var trainer = mlContext.BinaryClassification.Trainers.FastTree(label: "Label", features: "Features");
+
             //Other way: var trainer = new LinearClassificationTrainer(mlContext, "Features", "Label");
             modelBuilder.AddTrainer(trainer);
 
@@ -55,7 +56,7 @@ namespace SentimentAnalysisConsoleApp
             // STEP 5: Evaluate the model and show accuracy stats
             Console.WriteLine("===== Evaluating Model's accuracy with Test data =====");
             var metrics = modelBuilder.EvaluateBinaryClassificationModel(testDataView, "Label", "Score");
-            Common.ConsoleHelper.PrintBinaryClassificationMetrics("StochasticDualCoordinateAscent", metrics);
+            Common.ConsoleHelper.PrintBinaryClassificationMetrics("FastTree", metrics);
 
             // STEP 6: Save/persist the trained model to a .ZIP file
             Console.WriteLine("=============== Saving the model to a file ===============");
