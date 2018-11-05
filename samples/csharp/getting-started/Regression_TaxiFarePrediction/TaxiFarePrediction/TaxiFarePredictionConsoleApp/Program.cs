6388da8d044d16e9f38750d1d3a8b638a2e5a1c9
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-11-04 22:04:54 -0800

    Minor clean-up in TaxiFare sample

--- samples/csharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/TaxiFarePredictionConsoleApp/Program.cs
+++ samples/csharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/TaxiFarePredictionConsoleApp/Program.cs
@@ -45,26 +45,6 @@ namespace Regression_TaxiFarePrediction
             Console.ReadLine();
         }
 
-        private static TextLoader CreateTaxiFareDataFileLoader(MLContext mlcontext)
-        {
-            return new TextLoader(mlcontext,
-                                                      new TextLoader.Arguments()
-                                                      {
-                                                          Separator = ",",
-                                                          HasHeader = true,
-                                                          Column = new[]
-                                                          {
-                                                    new TextLoader.Column("VendorId", DataKind.Text, 0),
-                                                    new TextLoader.Column("RateCode", DataKind.Text, 1),
-                                                    new TextLoader.Column("PassengerCount", DataKind.R4, 2),
-                                                    new TextLoader.Column("TripTime", DataKind.R4, 3),
-                                                    new TextLoader.Column("TripDistance", DataKind.R4, 4),
-                                                    new TextLoader.Column("PaymentType", DataKind.Text, 5),
-                                                    new TextLoader.Column("FareAmount", DataKind.R4, 6)
-                                                          }
-                                                      });
-        }
-
         private static ITransformer BuildTrainEvaluateAndSaveModel(MLContext mlContext)
         {
             // STEP 1: Common data loading configuration
