e89462e8393562d5f5669075a1c0cb120592cf90
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-11-05 21:03:51 -0800

    Fixing confidence Interval for CV metrics should be calculated using t-distribution quantiles

--- samples/csharp/common/ConsoleHelper.cs
+++ samples/csharp/common/ConsoleHelper.cs
@@ -114,13 +114,13 @@ namespace Common
         {
             double average = values.Average();
             double sumOfSquaresOfDifferences = values.Select(val => (val - average) * (val - average)).Sum();
-            double standardDeviation = Math.Sqrt(sumOfSquaresOfDifferences / values.Count());
+            double standardDeviation = Math.Sqrt(sumOfSquaresOfDifferences / (values.Count()-1));
             return standardDeviation;
         }
 
         public static double CalculateConfidenceInterval95(IEnumerable<double> values)
         {
-            double confidenceInterval95 = 1.96 * CalculateStandardDeviation(values) / Math.Sqrt(values.Count());
+            double confidenceInterval95 = 1.96 * CalculateStandardDeviation(values) / Math.Sqrt((values.Count()-1));
             return confidenceInterval95;
         }
 
