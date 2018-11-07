7762ca38715c4904fb78ccd703a57efee3875382
mariusz.wojcik@hotmail.co.uk <mariusz.wojcik@hotmail.co.uk>
2018-11-07 20:18:42 +0000

    Updated Clustering_Iris F# project to v0.7 and new project structure.

---
+++ samples/fsharp/getting-started/Clustering_Iris/IrisClustering/IrisClusteringConsoleApp/DataStructures/DataStructures.fs
@@ -0,0 +1,21 @@
+﻿namespace Clustering_Iris.DataStructures
+
+module DataStructures =
+    open Microsoft.ML.Runtime.Api
+
+    /// Describes Iris flower. Used as an input to prediction function.
+    [<CLIMutable>]
+    type IrisData = {
+        SepalLength : float32
+        SepalWidth: float32
+        PetalLength : float32
+        PetalWidth : float32
+    } 
+
+
+    /// Represents result of prediction - the cluster to which Iris flower has been classified.
+    [<CLIMutable>]
+    type IrisPrediction = {
+        [<ColumnName("PredictedLabel")>] SelectedClusterId : uint32
+        [<ColumnName("Score")>] Distance : float32[]
+    }
