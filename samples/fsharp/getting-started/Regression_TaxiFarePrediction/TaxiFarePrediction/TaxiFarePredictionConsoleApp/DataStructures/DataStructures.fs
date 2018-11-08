a5402292086bc061edaf0c82be56dcfbfc7d72b4
mariusz.wojcik@hotmail.co.uk <mariusz.wojcik@hotmail.co.uk>
2018-11-08 06:27:37 +0000

    Removed unused Taxi trip from DataStructure

--- samples/fsharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/TaxiFarePredictionConsoleApp/DataStructures/DataStructures.fs
+++ samples/fsharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/TaxiFarePredictionConsoleApp/DataStructures/DataStructures.fs
@@ -19,14 +19,3 @@ module DataStructures =
         [<ColumnName("Score")>]
         FareAmount : float32
     }
-
-    let Trip1 =
-        {
-            VendorId = "VTS"
-            RateCode = "1"
-            PassengerCount = 1.0f
-            TripTime = 0.0f
-            TripDistance = 10.33f
-            PaymentType = "CSH"
-            FareAmount = 0.0f // predict it. actual = 29.5
-        }
