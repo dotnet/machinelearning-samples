4fed37e68568193f740bd3716c230cbf5700dd48
mariusz.wojcik@hotmail.co.uk <mariusz.wojcik@hotmail.co.uk>
2018-11-21 06:23:04 +0000

    #117: migrated end-to-end MulticlassClassification-GitHubLabeler sample to F#

---
+++ samples/fsharp/end-to-end-apps/MulticlassClassification-GitHubLabeler/GitHubLabeler/GitHubLabelerConsoleApp/GitHubClientFacade.fs
@@ -0,0 +1,18 @@
+﻿module GitHubClientFacade
+
+
+open System
+open Octokit
+
+let getAllIssuesForRepository (client : GitHubClient) repoName repoOwner (issueRequest : RepositoryIssueRequest) =
+    client.Issue.GetAllForRepository(repoOwner, repoName, issueRequest)
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+    |> Seq.toList
+
+
+let updateIssue (client : GitHubClient) repoName repoOwner (issue : Issue) issueUpdate =
+    client.Issue.Update(repoOwner, repoName, issue.Number, issueUpdate)
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+    |> ignore
---
+++ samples/fsharp/end-to-end-apps/MulticlassClassification-GitHubLabeler/GitHubLabeler/GitHubLabelerConsoleApp/GitHubLabelerDataProcessPipelineFactory.fs
@@ -0,0 +1,11 @@
+﻿module GitHubLabelerDataProcessPipelineFactory
+
+open Microsoft.ML
+
+let createDataProcessPipeline (mlContext : MLContext) =
+
+    mlContext.Transforms.Categorical.MapValueToKey("Area", "Label")
+    |> Common.ModelBuilder.append (mlContext.Transforms.Text.FeaturizeText("Title", "TitleFeaturized"))
+    |> Common.ModelBuilder.append (mlContext.Transforms.Text.FeaturizeText("Description", "DescriptionFeaturized"))
+    |> Common.ModelBuilder.append (mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
+    |> Common.ModelBuilder.downcastPipeline
