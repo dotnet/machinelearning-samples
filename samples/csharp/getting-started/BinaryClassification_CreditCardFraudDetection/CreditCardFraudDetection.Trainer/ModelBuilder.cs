bf1d89e4e9dfb773181007f57ab8b9b1bfd0cbec
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-11-21 19:43:28 -0800

    FraudDetection sample refactored not to use the BinarySaver and lower level HostEnvironment but instead the MLContext.Data.SaveAsText()

--- samples/csharp/getting-started/BinaryClassification_CreditCardFraudDetection/CreditCardFraudDetection.Trainer/ModelBuilder.cs
+++ samples/csharp/getting-started/BinaryClassification_CreditCardFraudDetection/CreditCardFraudDetection.Trainer/ModelBuilder.cs
@@ -82,12 +82,7 @@ namespace CreditCardFraudDetection.Trainer
             IDataView trainData = null;
             IDataView testData = null;
 
-            // Step one: read the data as an IDataView.
-            // Create the reader: define the data columns 
-            // and where to find them in the text file.
-            var reader = new TextLoader(mlContext, new TextLoader.Arguments
-            {
-                Column = new[] {
+            TextLoader.Column[] columns = new[] {
                     // A boolean column depicting the 'label'.
                     new TextLoader.Column("Label", DataKind.BL, 30),
                     // 29 Features V1..V28 + Amount
@@ -119,12 +114,21 @@ namespace CreditCardFraudDetection.Trainer
                     new TextLoader.Column("V26", DataKind.R4, 26 ),
                     new TextLoader.Column("V27", DataKind.R4, 27 ),
                     new TextLoader.Column("V28", DataKind.R4, 28 ),
-                    new TextLoader.Column("Amount", DataKind.R4, 29 ),
-                },
-                // First line of the file is a header, not a data row.
-                HasHeader = true,
-                Separator = ","
-            });
+                    new TextLoader.Column("Amount", DataKind.R4, 29 )
+                };
+
+            TextLoader.Arguments txtLoaderArgs = new TextLoader.Arguments
+                                                        {
+                                                            Column = columns,
+                                                            // First line of the file is a header, not a data row.
+                                                            HasHeader = true,
+                                                            Separator = ","
+                                                        };
+
+            // Step one: read the data as an IDataView.
+            // Create the reader: define the data columns 
+            // and where to find them in the text file.
+            var reader = new TextLoader(mlContext, txtLoaderArgs);
 
 
             // We know that this is a Binary Classification task,
@@ -134,7 +138,7 @@ namespace CreditCardFraudDetection.Trainer
             var classification = new BinaryClassificationContext(mlContext);
 
             if (!File.Exists(Path.Combine(_outputPath, "testData.idv")) &&
-                !File.Exists(Path.Combine(_outputPath, "trainData.idv"))){
+                !File.Exists(Path.Combine(_outputPath, "trainData.idv"))) {
                 // Split the data 80:20 into train and test sets, train and evaluate.
 
                 data = reader.Read(new MultiFileSource(_dataSetFile));
@@ -148,33 +152,72 @@ namespace CreditCardFraudDetection.Trainer
                 (trainData, testData) = classification.TrainTestSplit(data, testFraction: 0.2);
 
                 // save test split
-                IHostEnvironment env = (IHostEnvironment)mlContext;
-                using (var ch = env.Start("SaveData"))
-                using (var file = env.CreateOutputFile(Path.Combine(_outputPath, "testData.idv")))
+                using (var fileStream = File.Create(Path.Combine(_outputPath, "testData.csv")))
                 {
-                    var saver = new BinarySaver(mlContext, new BinarySaver.Arguments());
-                    DataSaverUtils.SaveDataView(ch, saver, testData, file);
+                    mlContext.Data.SaveAsText(testData, fileStream, separator:',', headerRow:true, schema: true);
                 }
 
-                // save train split
-                using (var ch = ((IHostEnvironment)env).Start("SaveData"))
-                using (var file = env.CreateOutputFile(Path.Combine(_outputPath, "trainData.idv")))
+                // save train split 
+                using (var fileStream = File.Create(Path.Combine(_outputPath, "trainData.csv")))
                 {
-                    var saver = new BinarySaver(mlContext, new BinarySaver.Arguments());
-                    DataSaverUtils.SaveDataView(ch, saver, trainData, file);
+                    mlContext.Data.SaveAsText(testData, fileStream, separator: ',', headerRow: true, schema: true);
                 }
 
             }
-            else {
+            else
+            {
+                //Add the "StratificationColumn" that was added by classification.TrainTestSplit()
+                // And Label is moved to column 0
+
+                TextLoader.Column[] columnsPlus = new[] {
+                    // A boolean column depicting the 'label'.
+                    new TextLoader.Column("Label", DataKind.BL, 0),
+                    // 30 Features V1..V28 + Amount + StratificationColumn
+                    new TextLoader.Column("V1", DataKind.R4, 1 ),
+                    new TextLoader.Column("V2", DataKind.R4, 2 ),
+                    new TextLoader.Column("V3", DataKind.R4, 3 ),
+                    new TextLoader.Column("V4", DataKind.R4, 4 ),
+                    new TextLoader.Column("V5", DataKind.R4, 5 ),
+                    new TextLoader.Column("V6", DataKind.R4, 6 ),
+                    new TextLoader.Column("V7", DataKind.R4, 7 ),
+                    new TextLoader.Column("V8", DataKind.R4, 8 ),
+                    new TextLoader.Column("V9", DataKind.R4, 9 ),
+                    new TextLoader.Column("V10", DataKind.R4, 10 ),
+                    new TextLoader.Column("V11", DataKind.R4, 11 ),
+                    new TextLoader.Column("V12", DataKind.R4, 12 ),
+                    new TextLoader.Column("V13", DataKind.R4, 13 ),
+                    new TextLoader.Column("V14", DataKind.R4, 14 ),
+                    new TextLoader.Column("V15", DataKind.R4, 15 ),
+                    new TextLoader.Column("V16", DataKind.R4, 16 ),
+                    new TextLoader.Column("V17", DataKind.R4, 17 ),
+                    new TextLoader.Column("V18", DataKind.R4, 18 ),
+                    new TextLoader.Column("V19", DataKind.R4, 19 ),
+                    new TextLoader.Column("V20", DataKind.R4, 20 ),
+                    new TextLoader.Column("V21", DataKind.R4, 21 ),
+                    new TextLoader.Column("V22", DataKind.R4, 22 ),
+                    new TextLoader.Column("V23", DataKind.R4, 23 ),
+                    new TextLoader.Column("V24", DataKind.R4, 24 ),
+                    new TextLoader.Column("V25", DataKind.R4, 25 ),
+                    new TextLoader.Column("V26", DataKind.R4, 26 ),
+                    new TextLoader.Column("V27", DataKind.R4, 27 ),
+                    new TextLoader.Column("V28", DataKind.R4, 28 ),
+                    new TextLoader.Column("Amount", DataKind.R4, 29 ),
+                    new TextLoader.Column("StratificationColumn", DataKind.R4, 30 )
+                };
+
                 // Load splited data
-                var binTrainData = new BinaryLoader(mlContext, new BinaryLoader.Arguments(), new MultiFileSource(Path.Combine(_outputPath, "trainData.idv")));
-                var trainRoles = new RoleMappedData(binTrainData, roles: TransactionObservation.Roles());
-                trainData = trainRoles.Data;
-
-
-                var binTestData = new BinaryLoader(mlContext, new BinaryLoader.Arguments(), new MultiFileSource(Path.Combine(_outputPath, "testData.idv")));
-                var testRoles = new RoleMappedData(binTestData, roles: TransactionObservation.Roles());
-                testData = testRoles.Data;
+                trainData = mlContext.Data.ReadFromTextFile(columnsPlus, Path.Combine(_outputPath, "trainData.csv"),
+                                                                              advancedSettings: s => {
+                                                                                  s.HasHeader = txtLoaderArgs.HasHeader;
+                                                                                  s.Separator = txtLoaderArgs.Separator;
+                                                                              }
+                                                                             );
+                testData = mlContext.Data.ReadFromTextFile(columnsPlus, Path.Combine(_outputPath, "testData.csv"),
+                                                                              advancedSettings: s => {
+                                                                                  s.HasHeader = txtLoaderArgs.HasHeader;
+                                                                                  s.Separator = txtLoaderArgs.Separator;
+                                                                              }
+                                                                             );
 
             }
 
