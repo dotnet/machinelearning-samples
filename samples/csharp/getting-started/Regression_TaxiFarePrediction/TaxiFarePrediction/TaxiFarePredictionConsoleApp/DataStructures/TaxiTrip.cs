6dfa957a4f193ce5a998125b3f580d95320ddee1
Shahab Moradi <shmoradi@microsoft.com>
2018-12-03 15:02:17 -0800

    Removed old annotations

--- samples/csharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/TaxiFarePredictionConsoleApp/DataStructures/TaxiTrip.cs
+++ samples/csharp/getting-started/Regression_TaxiFarePrediction/TaxiFarePrediction/TaxiFarePredictionConsoleApp/DataStructures/TaxiTrip.cs
@@ -4,25 +4,18 @@ namespace Regression_TaxiFarePrediction.DataStructures
 {
     public class TaxiTrip
     {
-        [Column("0")]
         public string VendorId;
 
-        [Column("1")]
         public string RateCode;
         
-        [Column("2")]
         public float PassengerCount;
         
-        [Column("3")]
         public float TripTime;
-        
-        [Column("4")]
+
         public float TripDistance;
         
-        [Column("5")]
         public string PaymentType;
-        
-        [Column("6")]
+
         public float FareAmount;
     }
 }
