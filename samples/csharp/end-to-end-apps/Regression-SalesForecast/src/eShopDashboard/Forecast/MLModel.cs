ba9b869b64714d2dc515341f811456dc79dfbd9b
Cesar De la Torre Llorente <cesardl@microsoft.com>
2018-12-18 18:18:43 -0800

    Object Pooling implementation for PredictionEngine objects which are not thread-safe

--- samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopDashboard/Forecast/MLModel.cs
+++ samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopDashboard/Forecast/MLModel.cs
@@ -13,29 +13,53 @@ namespace eShopDashboard.Forecast
         private readonly MLContext _mlContext;
         private readonly ITransformer _model;
         private readonly ObjectPool<PredictionFunction<TData, TPrediction>> _predictionEnginePool;
+        private readonly int _minPredictionEngineObjectsInPool;
 
-        //MLContext is injected through DI/IoC
-        public MLModel(MLContext mlContext, IConfiguration configuration)
+        //Constructor with modelFilePathName to load
+        public MLModel(MLContext mlContext, string modelFilePathName, int minPredictionEngineObjectsInPool = 10)
         {
             _mlContext = mlContext;
-            string modelFolder = configuration["ForecastModelsPath"];
 
             //Load the ProductSalesForecast model from the .ZIP file
-            using (var fileStream = File.OpenRead($"{modelFolder}/country_month_fastTreeTweedie.zip"))
+            using (var fileStream = File.OpenRead(modelFilePathName))
             {
                 _model = mlContext.Model.Load(fileStream);
             }
 
+            _minPredictionEngineObjectsInPool = minPredictionEngineObjectsInPool;
+
             //Create PredictionEngine Object Pool
-            _predictionEnginePool = new ObjectPool<PredictionFunction<TData, TPrediction>>(() => _model.MakePredictionFunction<TData, TPrediction>(_mlContext));
+            _predictionEnginePool = CreatePredictionEngineObjectPool();
         }
 
-        /// <summary>
-        /// This function creates a prediction function from the loaded model.
-        /// </summary>
-        public PredictionFunction<CountryData, CountrySalesPrediction> CreatePredictionFunction()
+        //Constructor with ITransformer model already created
+        public MLModel(MLContext mlContext, ITransformer model, int minPredictionEngineObjectsInPool = 10)
         {
-            return _model.MakePredictionFunction<CountryData, CountrySalesPrediction>(_mlContext);
+            _mlContext = mlContext;
+            _model = model;
+            _minPredictionEngineObjectsInPool = minPredictionEngineObjectsInPool;
+
+            //Create PredictionEngine Object Pool
+            _predictionEnginePool = CreatePredictionEngineObjectPool();
+        }
+
+        private ObjectPool<PredictionFunction<TData, TPrediction>> CreatePredictionEngineObjectPool()
+        {
+            return new ObjectPool<PredictionFunction<TData, TPrediction>>(() => _model.MakePredictionFunction<TData, TPrediction>(_mlContext), _minPredictionEngineObjectsInPool);
+        }
+
+        public TPrediction Predict(TData dataSample)
+        {
+            //Get PredictionEngine object from the Object Pool
+            PredictionFunction<TData, TPrediction> predictionEngine = _predictionEnginePool.GetObject();
+
+            //Predict
+            TPrediction prediction = predictionEngine.Predict(dataSample);
+
+            //Release used PredictionEngine object into the Object Pool
+            _predictionEnginePool.PutObject(predictionEngine);
+
+            return prediction;
         }
 
     }
--- samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopDashboard/Forecast/ObjectPool.cs
+++ samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopDashboard/Forecast/ObjectPool.cs
@@ -11,18 +11,33 @@ namespace eShopDashboard.Forecast
         private ConcurrentBag<T> _objects;
         private Func<T> _objectGenerator;
 
-        public ObjectPool(Func<T> objectGenerator)
+        public ObjectPool(Func<T> objectGenerator, int minObjects = 5)
         {
             if (objectGenerator == null) throw new ArgumentNullException("objectGenerator");
             _objects = new ConcurrentBag<T>();
             _objectGenerator = objectGenerator;
+          
+            //Measure total time of minimum objects creation 
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            //Create minimum number of objects in pool
+            for (int i = 0; i < minObjects; i++)
+            {
+                _objects.Add(_objectGenerator());
+            }
+
+            //Stop measuring time
+            watch.Stop();
+            long elapsedMs = watch.ElapsedMilliseconds;     
         }
 
         public T GetObject()
         {
             T item;
-            if (_objects.TryTake(out item)) return item;
-            return _objectGenerator();
+            if (_objects.TryTake(out item))
+                return item;
+            else
+                return _objectGenerator();
         }
 
         public void PutObject(T item)
