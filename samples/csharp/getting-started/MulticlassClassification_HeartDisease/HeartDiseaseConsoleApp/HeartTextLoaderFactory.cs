9c9be6200120e4fa7b65b21fe4760f5ce168c71c
Marco Zamana <marco.zamana1@gmail.com>
2019-01-24 22:42:02 +0100

    Update ML.NET to 0.9

--- samples/csharp/getting-started/MulticlassClassification_HeartDisease/HeartDiseaseConsoleApp/HeartTextLoaderFactory.cs
+++ samples/csharp/getting-started/MulticlassClassification_HeartDisease/HeartDiseaseConsoleApp/HeartTextLoaderFactory.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.ML;
-using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Data;
 
 namespace MulticlassClassification_HeartDisease
 {
@@ -7,29 +7,26 @@ namespace MulticlassClassification_HeartDisease
     {
         public static TextLoader CreateTextLoader(MLContext mlContext)
         {
-            TextLoader textLoader = mlContext.Data.TextReader(new Microsoft.ML.Runtime.Data.TextLoader.Arguments()
-            {
-                HasHeader = false,
-                Separator = ",",
-                Column = new[]
+            TextLoader textLoader = mlContext.Data.CreateTextReader(
+                columns:new[]
                 {
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Age", DataKind.R4, 0),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Sex", DataKind.R4, 1),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Cp", DataKind.R4, 2),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("TrestBps", DataKind.R4, 3),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Chol", DataKind.R4, 4),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Fbs", DataKind.R4, 5),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("RestEcg", DataKind.R4, 6),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Thalac", DataKind.R4, 7),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Exang", DataKind.R4, 8),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("OldPeak", DataKind.R4, 9),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Slope", DataKind.R4, 10),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Ca", DataKind.R4, 11),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Thal", DataKind.R4, 12),
-                    new Microsoft.ML.Runtime.Data.TextLoader.Column("Label", DataKind.R4, 13)
-                }
-
-            });
+                    new TextLoader.Column("Age", DataKind.R4, 0),
+                    new TextLoader.Column("Sex", DataKind.R4, 1),
+                    new TextLoader.Column("Cp", DataKind.R4, 2),
+                    new TextLoader.Column("TrestBps", DataKind.R4, 3),
+                    new TextLoader.Column("Chol", DataKind.R4, 4),
+                    new TextLoader.Column("Fbs", DataKind.R4, 5),
+                    new TextLoader.Column("RestEcg", DataKind.R4, 6),
+                    new TextLoader.Column("Thalac", DataKind.R4, 7),
+                    new TextLoader.Column("Exang", DataKind.R4, 8),
+                    new TextLoader.Column("OldPeak", DataKind.R4, 9),
+                    new TextLoader.Column("Slope", DataKind.R4, 10),
+                    new TextLoader.Column("Ca", DataKind.R4, 11),
+                    new TextLoader.Column("Thal", DataKind.R4, 12),
+                    new TextLoader.Column("Label", DataKind.R4, 13)
+                },
+                hasHeader:false,
+                separatorChar: ',');
 
             return textLoader;
         }
--- samples/csharp/getting-started/MulticlassClassification_HeartDisease/HeartDiseaseConsoleApp/Program.cs
+++ samples/csharp/getting-started/MulticlassClassification_HeartDisease/HeartDiseaseConsoleApp/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using Microsoft.ML;
+﻿using Microsoft.ML;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Data;
 using MulticlassClassification_HeartDisease.DataStructure;
+using System;
+using System.IO;
 
 namespace MulticlassClassification_HeartDisease
 {
     public class Program
     {
-        //private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
 
-        private static string BaseDatasetsLocation = @"../../../../HeartPrediction/DataFiles";
+        private static string BaseDatasetsLocation = @"../../../../DataFiles";
         private static string TrainDataPath = $"{BaseDatasetsLocation}/HeartTraining.csv";
         private static string TestDataPath = $"{BaseDatasetsLocation}/HeartTest.csv";
 
@@ -36,7 +39,7 @@ namespace MulticlassClassification_HeartDisease
             var testDataView = textLoader.Read(TestDataPath);
 
 
-            var dataProcess = mlContext.Transforms.Concatenate("Features",
+            var dataProcessPipeline = mlContext.Transforms.Concatenate("Features",
                 "Age",
                 "Sex", 
                 "Cp", 
@@ -52,24 +55,36 @@ namespace MulticlassClassification_HeartDisease
                 "Thal"
             );
 
-            var modelBuilder =
-                new Common.ModelBuilder<HeartData, MulticlassClassification_HeartDisease.DataStructure.HeartPrediction>(mlContext, dataProcess);
+            IEstimator<ITransformer> trainer = mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(DefaultColumnNames.Label,DefaultColumnNames.Features);
 
-            var trainer = mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent();
-            modelBuilder.AddTrainer(trainer);
+            trainer = mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent();
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
 
             Console.WriteLine("=============== Training the model ===============");
-            modelBuilder.Train(trainingDataView);
+            var trainedModel = trainingPipeline.Fit(trainingDataView);
             Console.WriteLine();
 
 
             Console.WriteLine("===== Evaluating Model's accuracy with Test data =====");
-            var metrics = modelBuilder.EvaluateMultiClassClassificationModel(testDataView, "Label");
-            Common.ConsoleHelper.PrintMultiClassClassificationMetrics(trainer.ToString(), metrics);
+            var metrics = mlContext.MulticlassClassification.Evaluate(trainingDataView, "Label", "score", "PredictedLabel", 0);
+
+
+            Console.WriteLine($"************************************************************");
+            Console.WriteLine($"*    Metrics for {trainer.ToString()} multi-class classification model   ");
+            Console.WriteLine($"*-----------------------------------------------------------");
+            Console.WriteLine($"    AccuracyMacro = {metrics.AccuracyMacro:0.####}, a value between 0 and 1, the closer to 1, the better");
+            Console.WriteLine($"    AccuracyMicro = {metrics.AccuracyMicro:0.####}, a value between 0 and 1, the closer to 1, the better");
+            Console.WriteLine($"    LogLoss = {metrics.LogLoss:0.####}, the closer to 0, the better");
+            Console.WriteLine($"    LogLoss for class 1 = {metrics.PerClassLogLoss[0]:0.####}, the closer to 0, the better");
+            Console.WriteLine($"    LogLoss for class 2 = {metrics.PerClassLogLoss[1]:0.####}, the closer to 0, the better");
+            Console.WriteLine($"    LogLoss for class 3 = {metrics.PerClassLogLoss[2]:0.####}, the closer to 0, the better");
+            Console.WriteLine($"************************************************************");
             Console.WriteLine();
 
             Console.WriteLine("=============== Saving the model to a file ===============");
-            modelBuilder.SaveModelAsFile(ModelPath);
+            using (var fs = new FileStream(ModelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                mlContext.Model.Save(trainedModel, fs);
+
             Console.WriteLine();
 
         }
@@ -77,19 +92,23 @@ namespace MulticlassClassification_HeartDisease
 
         private static void TestPrediction(MLContext mlContext)
         {
-            var modelScorer = new Common.ModelScorer<HeartData, MulticlassClassification_HeartDisease.DataStructure.HeartPrediction>(mlContext);
-            modelScorer.LoadModelFromZipFile(ModelPath);
-         
+            ITransformer trainedModel;
+
+            using (var stream = new FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                trainedModel = mlContext.Model.Load(stream);
+            }
+            var predictionEngine = trainedModel.CreatePredictionEngine<HeartData, HeartPrediction>(mlContext);
+
 
             foreach (var heartData in HeartSampleData.heartDatas)
             {
-                var prediction = modelScorer.PredictSingle(heartData);
+                var prediction = predictionEngine.Predict(heartData);
 
-                Console.WriteLine($" 0: {prediction.Score[0]:0.###}");
+                Console.WriteLine($" 0: {prediction.Score:0.###}");
                 Console.WriteLine($" 1: {prediction.Score[1]:0.###}");
                 Console.WriteLine($" 2: {prediction.Score[2]:0.###}");
                 Console.WriteLine($" 3: {prediction.Score[3]:0.###}");
-                Console.WriteLine($" 4: {prediction.Score[4]:0.###}");
                 Console.WriteLine();
 
             }
