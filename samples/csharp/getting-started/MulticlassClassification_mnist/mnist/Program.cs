c2cb24e99e9a294227930e4e503515dffd8fc725
ashisa <ashisa78@live.com>
2019-01-28 18:38:52 +0530

    update README/code

--- samples/csharp/getting-started/MulticlassClassification_mnist/mnist/Program.cs
+++ samples/csharp/getting-started/MulticlassClassification_mnist/mnist/Program.cs
@@ -12,9 +12,9 @@ namespace mnist
     class Program
     {
 
-        static readonly string _datapath = Path.Combine(Environment.CurrentDirectory, "Data", "optdigits-train.csv");
-        static readonly string _testdatapath = Path.Combine(Environment.CurrentDirectory, "Data", "optdigits-val.csv");
-        static readonly string _modelpath = Path.Combine(Environment.CurrentDirectory, "MLModels", "Model.zip");
+        static readonly string TrainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "optdigits-train.csv");
+        static readonly string ValidationDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "optdigits-val.csv");
+        static readonly string ModelPath = Path.Combine(Environment.CurrentDirectory, "MLModels", "Model.zip");
 
         static void Main(string[] args)
         {
@@ -30,14 +30,30 @@ namespace mnist
 
                 var classification = new MulticlassClassificationContext(env);
 
+                // STEP 1: Common data loading configuration
+                var reader = env.Data.CreateTextReader(
+                    new TextLoader.Arguments()
+                    {
+                        Column = new[] 
+                        {
+                            new TextLoader.Column("PixelValues", DataKind.R4, 0, 63),
+                            new TextLoader.Column("Number", DataKind.R4, 64)
+                        },
+                        Separator = ",",
+                        HasHeader = false
+                    });
 
-                var reader = env.Data.CreateTextReader(new TextLoader.Arguments { Column = new[] { new TextLoader.Column("PixelValues", DataKind.R4, 0, 63), new TextLoader.Column("Number", DataKind.R4, 64) }, Separator = ",", HasHeader = false });
-                var data = reader.Read(_datapath);
-                var testData = reader.Read(_testdatapath);
+                var data = reader.Read(TrainDataPath);
+                var testData = reader.Read(ValidationDataPath);
 
+                // STEP 2: Common data process configuration with pipeline data transformations
                 var dataProcessPipeline = env.Transforms.Concatenate("Features", "PixelValues").AppendCacheCheckpoint(env);
+
+                // STEP 3: Set the training algorithm, then create and config the modelBuilder
                 var trainer = env.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(labelColumn: "Number", featureColumn: "Features");
                 var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+                // STEP 4: Train the model fitting to the DataSet
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 Console.WriteLine("=============== Training the model ===============");
 
@@ -49,14 +65,10 @@ namespace mnist
                 var predictions = trainedModel.Transform(testData);
                 var metrics = env.MulticlassClassification.Evaluate(predictions, "Number", "Score");
 
-
-                using (var fs = new FileStream(_modelpath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                using (var fs = new FileStream(ModelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
                     env.Model.Save(trainedModel, fs);
 
-                Console.WriteLine("The model is saved to {0}", _modelpath);
-
-
-
+                Console.WriteLine("The model is saved to {0}", ModelPath);
             }
             catch (Exception ex)
             {
@@ -68,9 +80,8 @@ namespace mnist
 
         private static void TestSomePredictions(MLContext mlContext)
         {
-
             ITransformer trainedModel;
-            using (var stream = new FileStream(_modelpath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var stream = new FileStream(ModelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
                 trainedModel = mlContext.Model.Load(stream);
             }
@@ -78,11 +89,10 @@ namespace mnist
             // Create prediction engine related to the loaded trained model
             var predEngine = trainedModel.CreatePredictionEngine<InputData, OutPutNum>(mlContext);
 
+            //InputData data1 = SampleMNISTData.MNIST1;
+            var resultprediction1 = predEngine.Predict(SampleMNISTData.MNIST1);
 
-            InputData data1 = SampleMNISTData.MNIST1;
-            var resultprediction1 = predEngine.Predict(data1);
-
-            Console.WriteLine($"Actual:   1   Predicted probability:        zero:  {resultprediction1.Score[0]:0.####}");
+            Console.WriteLine($"Actual: 1     Predicted probability:       zero:  {resultprediction1.Score[0]:0.####}");
             Console.WriteLine($"                                           One :  {resultprediction1.Score[1]:0.####}");
             Console.WriteLine($"                                           two:   {resultprediction1.Score[2]:0.####}");
             Console.WriteLine($"                                           three: {resultprediction1.Score[3]:0.####}");
@@ -94,11 +104,10 @@ namespace mnist
             Console.WriteLine($"                                           nine:  {resultprediction1.Score[9]:0.####}");
             Console.WriteLine();
 
+            //InputData data2 = SampleMNISTData.MNIST2;
+            var resultprediction2 = predEngine.Predict(SampleMNISTData.MNIST2);
 
-            InputData data2 = SampleMNISTData.MNIST2;
-            var resultprediction2 = predEngine.Predict(data2);
-
-            Console.WriteLine($"Actual:  7    Predicted probability:        zero:  {resultprediction2.Score[0]:0.####}");
+            Console.WriteLine($"Actual: 7     Predicted probability:       zero:  {resultprediction2.Score[0]:0.####}");
             Console.WriteLine($"                                           One :  {resultprediction2.Score[1]:0.####}");
             Console.WriteLine($"                                           two:   {resultprediction2.Score[2]:0.####}");
             Console.WriteLine($"                                           three: {resultprediction2.Score[3]:0.####}");
@@ -110,12 +119,10 @@ namespace mnist
             Console.WriteLine($"                                           nine:  {resultprediction2.Score[9]:0.####}");
             Console.WriteLine();
 
+            //InputData data3 = SampleMNISTData.MNIST3;
+            var resultprediction3 = predEngine.Predict(SampleMNISTData.MNIST3);
 
-
-            InputData data3 = SampleMNISTData.MNIST3;
-            var resultprediction3 = predEngine.Predict(data3);
-
-            Console.WriteLine($"Actual:   9   Predicted probability:        zero:  {resultprediction3.Score[0]:0.####}");
+            Console.WriteLine($"Actual: 9     Predicted probability:       zero:  {resultprediction3.Score[0]:0.####}");
             Console.WriteLine($"                                           One :  {resultprediction3.Score[1]:0.####}");
             Console.WriteLine($"                                           two:   {resultprediction3.Score[2]:0.####}");
             Console.WriteLine($"                                           three: {resultprediction3.Score[3]:0.####}");
@@ -126,8 +133,6 @@ namespace mnist
             Console.WriteLine($"                                           eight: {resultprediction3.Score[8]:0.####}");
             Console.WriteLine($"                                           nine:  {resultprediction3.Score[9]:0.####}");
             Console.WriteLine();
-
-
         }
     }
 }
