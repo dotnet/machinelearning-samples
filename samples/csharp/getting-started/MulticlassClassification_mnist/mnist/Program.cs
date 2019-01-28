3a57f2562ad5bad380ad7ea5874672172ae22780
ashisa <ashisa78@live.com>
2019-01-28 15:51:40 +0530

    add mnist solution

---
+++ samples/csharp/getting-started/MulticlassClassification_mnist/mnist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mnist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
