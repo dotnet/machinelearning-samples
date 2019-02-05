9d173234083801057496267d25a1a670012093ba
King David Consulting LLC <info@kingdavidconsulting.com>
2019-02-04 19:23:44 -0500

    Update MovieRecommender sample (#148)
    * add sefl update script
    
    * update some of the code
    
    * add .editorconfig file to enforce the consistency of the rules.
    
    * rollback var to actual types
    
    * initial attempt to migrate
    
    * working copy of the model building code
    
    * upgraded version of the code
    
    * upgrade to the latest ML.NET
    
    * revert example to Field Aware Factorization Machines

---
+++ samples/csharp/end-to-end-apps/Recommendation-MovieRecommender/MovieRecommender/movierecommender/Services/IProfileService.cs
@@ -0,0 +1,14 @@
+﻿using movierecommender.Models;
+using System.Collections.Generic;
+
+namespace movierecommender.Services
+{
+    public interface IProfileService
+    {
+        Profile GetProfileByID(int id);
+
+        List<(int movieId, int movieRating)> GetProfileWatchedMovies(int id);
+
+        List<Profile> GetProfiles { get; }
+    }
+}
