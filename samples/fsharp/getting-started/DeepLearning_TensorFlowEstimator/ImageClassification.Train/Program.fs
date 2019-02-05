04ecc60f62449aba54678014455838edf193c88a
kevmal <kevin@xaai.ch>
2019-02-05 10:34:22 -0700

    F# deeplearning samples

---
+++ samples/fsharp/getting-started/DeepLearning_TensorFlowEstimator/ImageClassification.Train/Program.fs
@@ -0,0 +1,132 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open Microsoft.ML
+open Microsoft.ML.Data
+open Common
+open Microsoft.ML.ImageAnalytics
+
+let dataRoot = FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location)
+
+let imageHeight = 224
+let imageWidth = 224
+let mean = 117
+let scale = 1
+let channelsLast = true
+
+[<CLIMutable>]
+type ImageNetData = 
+    {
+        [<LoadColumn(0)>]
+        ImagePath : string
+        [<LoadColumn(1)>]
+        Label : string
+    }
+[<CLIMutable>]
+type ImageNetPipeline =
+    {
+        ImagePath : string
+        Label : string 
+        PredictedLabelValue : string 
+        Score : float32 []
+        softmax2_pre_activation : float32 []
+    }
+
+let printImagePrediction (x : ImageNetPipeline) = 
+    let defaultForeground = Console.ForegroundColor
+    let labelColor = ConsoleColor.Magenta
+    let probColor = ConsoleColor.Blue
+    printf "ImagePath: "
+    Console.ForegroundColor <- labelColor
+    printf "%s" (Path.GetFileName(x.ImagePath))
+    Console.ForegroundColor <- defaultForeground
+    printf " predicted as "
+    Console.ForegroundColor <- labelColor
+    printf "%s" x.PredictedLabelValue
+    Console.ForegroundColor <- defaultForeground
+    Console.Write(" with score ")
+    Console.ForegroundColor <- probColor
+    printf "%f" (x.Score |> Seq.max)
+    Console.ForegroundColor <- defaultForeground;
+    printfn ""
+
+let printHeader lines = 
+    let defaultColor = Console.ForegroundColor
+    Console.ForegroundColor <- ConsoleColor.Yellow
+    printfn " "
+    lines |> Seq.iter (printfn "%s")
+    let maxLength = lines |> Seq.map (fun x -> x.Length) |> Seq.max
+    printfn "%s" (String('#', maxLength))
+    Console.ForegroundColor <- defaultColor
+    
+let printExn lines = 
+    let defaultColor = Console.ForegroundColor
+    Console.ForegroundColor <- ConsoleColor.Red
+    printfn " "
+    printfn "EXCEPTION"
+    printfn "#########"
+    Console.ForegroundColor <- defaultColor
+    lines |> Seq.iter (printfn "%s")
+    
+
+let buildAndTrainModel dataLocation imagesFolder inputModelLocation imageClassifierZip = 
+    printfn "Read model"
+    printfn "Model location: %s" inputModelLocation
+    printfn "Images folder: %s" imagesFolder
+    printfn "Training file: %s" dataLocation
+    printfn "Default parameters: image size =(%d,%d), image mean: %d" imageHeight imageWidth mean
+    let mlContext = MLContext(seed = Nullable 1)
+    let data = mlContext.Data.ReadFromTextFile<ImageNetData>(dataLocation, hasHeader = false)
+    let pipeline =
+        mlContext.Transforms.Conversion.MapValueToKey("Label", "LabelTokey")
+        |> ModelBuilder.append (mlContext.Transforms.LoadImages(imagesFolder, struct ("ImagePath", "ImageReal")))
+        |> ModelBuilder.append (mlContext.Transforms.Resize("ImageReal", "ImageReal", imageHeight, imageWidth))
+        |> ModelBuilder.append (mlContext.Transforms.ExtractPixels(ImagePixelExtractorTransform.ColumnInfo("ImageReal", "input", interleave = channelsLast, offset = float32 mean)))
+        |> ModelBuilder.append (mlContext.Transforms.ScoreTensorFlowModel(inputModelLocation, [| "input" |], [| "softmax2_pre_activation" |]))
+        |> ModelBuilder.append (mlContext.MulticlassClassification.Trainers.LogisticRegression("LabelTokey", "softmax2_pre_activation"))
+        |> ModelBuilder.append (mlContext.Transforms.Conversion.MapKeyToValue(struct("PredictedLabel", "PredictedLabelValue")))
+    
+    printHeader ["Training classification model"]
+    let model = pipeline.Fit(data)
+    let trainData = model.Transform(data)
+    trainData.AsEnumerable<_>(mlContext, false, true)
+    |> Seq.iter printImagePrediction
+    let sdcaContext = MulticlassClassificationContext(mlContext)
+    
+    printHeader ["Classification metrics"]
+    let metrics = sdcaContext.Evaluate(trainData, label = "LabelTokey", predictedLabel = "PredictedLabel")
+    printfn "LogLoss is: %.15f" metrics.LogLoss
+    metrics.PerClassLogLoss
+    |> Seq.map string
+    |> String.concat " , "
+    |> printfn "PerClassLogLoss is: %s"
+    
+    printHeader ["Save model to local file"]
+    let outFile = Path.Combine(dataRoot.Directory.FullName, imageClassifierZip)
+    if File.Exists outFile then 
+        File.Delete(outFile)
+    do 
+        use f = File.OpenWrite(outFile)
+        mlContext.Model.Save(model, f)
+    printfn "Model saved: %s" outFile
+
+[<EntryPoint>]
+let main _argv =
+    let assetsPath = Path.Combine(dataRoot.Directory.FullName, @"..\..\..\assets")
+    let tagsTsv = Path.Combine(assetsPath, "inputs", "data", "tags.tsv")
+    let imagesFolder = Path.Combine(assetsPath, "inputs", "data")
+    let inceptionPb = Path.Combine(assetsPath, "inputs", "inception", "tensorflow_inception_graph.pb")
+    let imageClassifierZip = Path.Combine(assetsPath, "outputs", "imageClassifier.zip")
+    try
+        buildAndTrainModel tagsTsv imagesFolder inceptionPb imageClassifierZip
+    with 
+    | e -> printExn [e.Message]
+    
+    let defaultColor = Console.ForegroundColor
+    Console.ForegroundColor <- ConsoleColor.Green
+    printfn " "
+    printfn "Press any key to finish."
+    Console.ForegroundColor <- defaultColor
+    Console.ReadKey() |> ignore
+    0
