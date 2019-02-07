e51f1265b782a854f1a8306a3993ab9d2ff6ad81
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-02-07 05:10:10 -0800

    Migrated SalesForecast to v0.10 by fixing the below breaking changes and some refactoring. 1. Removed Build.Props file which ahs version 0.9 3.Changed the code according to ML.Net 0.10 API changes. 4.Specified parameter names in the ML.Net API calls, used DefaultColumnNames class to access constant names like Feature,Label, score etc. 5.Changed Readme file.

--- samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopForecastModelsTrainer/ProductModelHelper.cs
+++ samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopForecastModelsTrainer/ProductModelHelper.cs
@@ -11,6 +11,10 @@ namespace eShopForecastModelsTrainer
 {
     public class ProductModelHelper
     {
+        private static string NumFeatures = nameof(NumFeatures);
+
+        private static string CatFeatures = nameof(CatFeatures);
+
         /// <summary>
         /// Train and save model for predicting next month country unit sales
         /// </summary>
@@ -38,18 +42,19 @@ namespace eShopForecastModelsTrainer
 
             var trainingDataView = mlContext.Data.ReadFromTextFile<ProductData>(dataPath, hasHeader: true, separatorChar:',');
 
-            var trainer = mlContext.Regression.Trainers.FastTreeTweedie("Label", "Features");
+            var trainer = mlContext.Regression.Trainers.FastTreeTweedie(labelColumn: DefaultColumnNames.Label, featureColumn: DefaultColumnNames.Features);
 
-            var trainingPipeline = mlContext.Transforms.Concatenate(outputColumn: "NumFeatures", "year", "month", "units", "avg", "count", "max", "min", "prev" )
-                .Append(mlContext.Transforms.Categorical.OneHotEncoding(inputColumn:"productId", outputColumn:"CatFeatures"))
-                .Append(mlContext.Transforms.Concatenate(outputColumn: "Features", "NumFeatures", "CatFeatures"))
-                .Append(mlContext.Transforms.CopyColumns("next", "Label"))
+            var trainingPipeline = mlContext.Transforms.Concatenate(outputColumnName: NumFeatures, nameof(ProductData.year), nameof(ProductData.month), nameof(ProductData.units), nameof(ProductData.avg), nameof(ProductData.count), 
+                nameof(ProductData.max), nameof(ProductData.min), nameof(ProductData.prev) )
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: CatFeatures, inputColumnName: nameof(ProductData.productId)))
+                .Append(mlContext.Transforms.Concatenate(outputColumnName: DefaultColumnNames.Features, NumFeatures, CatFeatures))
+                .Append(mlContext.Transforms.CopyColumns(outputColumnName: DefaultColumnNames.Label, inputColumnName: nameof(ProductData.next)))
                 .Append(trainer);
             
             // Cross-Validate with single dataset (since we don't have two datasets, one for training and for evaluate)
             // in order to evaluate and get the model's accuracy metrics
             Console.WriteLine("=============== Cross-validating to get model's accuracy metrics ===============");
-            var crossValidationResults = mlContext.Regression.CrossValidate(trainingDataView, trainingPipeline, numFolds: 6, labelColumn: "Label");
+            var crossValidationResults = mlContext.Regression.CrossValidate(data:trainingDataView, estimator:trainingPipeline, numFolds: 6, labelColumn: DefaultColumnNames.Label);
             ConsoleHelper.PrintRegressionFoldsAverageMetrics(trainer.ToString(), crossValidationResults);
 
             // Train the model
