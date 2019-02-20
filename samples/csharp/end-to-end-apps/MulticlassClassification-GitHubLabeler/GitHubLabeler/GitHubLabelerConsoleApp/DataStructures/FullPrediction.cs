23a1fa585654041c881ea672928c71c8f0ce9baa
prathyusha12345 <prathyusha.krpt@gmail.com>
2019-02-19 17:55:12 -0800

    Added  code to classify github issues into the best 3 labels. (#260)
    1.Created a class to hold Prediction values
    2.Added Score filed in GitHubIssuePrediction class.
    3.Changed the existing code in Labeler class.
    4.Added new method to find the best 3 scores and return their indexes so that we can get the labels of those matched indexes from slotnames.

---
+++ samples/csharp/end-to-end-apps/MulticlassClassification-GitHubLabeler/GitHubLabeler/GitHubLabelerConsoleApp/DataStructures/FullPrediction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubLabeler.DataStructures
+{
+    public class FullPrediction
+    {
+        public string PredictedLabel;
+        public float Score;
+        public int OriginalSchemaIndex;
+
+        public FullPrediction(string predictedLabel, float score, int originalSchemaIndex)
+        {
+            PredictedLabel = predictedLabel;
+            Score = score;
+            OriginalSchemaIndex = originalSchemaIndex;
+        }
+    }
+}
