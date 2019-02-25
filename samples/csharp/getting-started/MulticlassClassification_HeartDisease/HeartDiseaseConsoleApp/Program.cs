f567dde92ddb6f36caf76e6914204c33f32aca5d
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-02-25 10:53:46 -0800

    Removed + symbol in path.Combine of HeartDisesase Sample as it is not required.

--- samples/csharp/getting-started/MulticlassClassification_HeartDisease/HeartDiseaseConsoleApp/Program.cs
+++ samples/csharp/getting-started/MulticlassClassification_HeartDisease/HeartDiseaseConsoleApp/Program.cs
@@ -100,7 +100,7 @@ namespace MulticlassClassification_HeartDisease
             FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
             string assemblyFolderPath = _dataRoot.Directory.FullName;
 
-            string fullPath = Path.Combine(assemblyFolderPath + "/" + relativeDatasetPath);
+            string fullPath = Path.Combine(assemblyFolderPath, relativeDatasetPath);
 
             return fullPath;
         }
