b5f6d0d6d530e75c6fe014cd9a1e33894aef373b
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-02-25 01:02:18 -0800

    Refactored code of Path Issue fix in SalesForecast sample

--- samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopForecastModelsTrainer/Program.cs
+++ samples/csharp/end-to-end-apps/Regression-SalesForecast/src/eShopForecastModelsTrainer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.ML;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using static eShopForecastModelsTrainer.ConsoleHelpers;
 
@@ -7,22 +8,40 @@ namespace eShopForecastModelsTrainer
 {
     class Program
     {
+        private static readonly string BaseDatasetsRelativePath = @"../../../../Data";
+        private static readonly string CountryDataRealtivePath = $"{BaseDatasetsRelativePath}/countries.stats.csv";
+        private static readonly string ProductDataRealtivePath = $"{BaseDatasetsRelativePath}/products.stats.csv";
+
+        private static readonly string CountryDataPath = GetDataSetAbsolutePath(CountryDataRealtivePath);
+        private static readonly string ProductDataPath = GetDataSetAbsolutePath(ProductDataRealtivePath);
+
         static void Main(string[] args)
         {
+            
             try
             {
                 MLContext mlContext = new MLContext(seed: 1);  //Seed set to any number so you have a deterministic environment
-
-                ProductModelHelper.TrainAndSaveModel(mlContext, "data/products.stats.csv");
+                
+                ProductModelHelper.TrainAndSaveModel(mlContext, ProductDataPath);
                 ProductModelHelper.TestPrediction(mlContext);
 
-                CountryModelHelper.TrainAndSaveModel(mlContext, "data/countries.stats.csv");
+                CountryModelHelper.TrainAndSaveModel(mlContext, CountryDataPath);
                 CountryModelHelper.TestPrediction(mlContext);
             } catch(Exception ex)
             {
                 ConsoleWriteException(ex.Message);
             }
             ConsolePressAnyKey();
+        }
+
+        public static string GetDataSetAbsolutePath(string relativeDatasetPath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath + "/" + relativeDatasetPath);
+
+            return fullPath;
         }
     }
 }
