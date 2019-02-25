7454d11be0f73abde83836208cc4fa7b4c689fb4
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-02-25 02:17:39 -0800

    Remvoed Console statements.

--- samples/csharp/getting-started/BinaryClassification_CreditCardFraudDetection/CreditCardFraudDetection.Trainer/Program.cs
+++ samples/csharp/getting-started/BinaryClassification_CreditCardFraudDetection/CreditCardFraudDetection.Trainer/Program.cs
@@ -44,10 +44,8 @@ namespace CreditCardFraudDetection.Trainer
         {
             FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
             string assemblyFolderPath = _dataRoot.Directory.FullName;
-            Console.WriteLine($"Assembly Folder Path: " + assemblyFolderPath);
 
             string fullPath = Path.Combine(assemblyFolderPath + "/" + relativeDatasetPath);
-            Console.WriteLine("\n" + $"Full Path: " + fullPath + "\n");
 
             return fullPath;
         }
