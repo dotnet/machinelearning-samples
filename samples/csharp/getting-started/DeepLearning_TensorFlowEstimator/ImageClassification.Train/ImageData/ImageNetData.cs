2f9f0ac633565b0e9825118a0073e4b5eb7aac54
Cesar De la Torre Llorente <cesardl@microsoft.com>
2019-03-22 16:35:47 -0700

    Minor refactoring on data classes on the TF predictor

--- samples/csharp/getting-started/DeepLearning_TensorFlowEstimator/ImageClassification.Train/ImageData/ImageNetData.cs
+++ samples/csharp/getting-started/DeepLearning_TensorFlowEstimator/ImageClassification.Train/ImageData/ImageNetData.cs
@@ -25,34 +25,4 @@ namespace ImageClassification.ImageData
         }
     }
 
-    public class ImageNetDataProbability : ImageNetData
-    {
-        public float Probability { get; set; }
-
-
-        public void ConsoleWriteLine()
-        {
-            var defaultForeground = Console.ForegroundColor;
-            var labelColor = ConsoleColor.Green;
-
-            Console.Write($"ImagePath: {ImagePath} predicted as ");
-            Console.ForegroundColor = labelColor;
-            Console.Write(Label);
-            Console.ForegroundColor = defaultForeground;
-            Console.Write(" with probability ");
-            Console.ForegroundColor = labelColor;
-            Console.Write(Probability);
-            Console.ForegroundColor = defaultForeground;
-            Console.WriteLine("");
-        }
-    }
-
-    public class ImageNetPipeline
-    {
-        public string ImagePath;
-        public string Label;
-        public string PredictedLabelValue;
-        public float[] Score;
-        public float[] softmax2_pre_activation;
-    }
 }
