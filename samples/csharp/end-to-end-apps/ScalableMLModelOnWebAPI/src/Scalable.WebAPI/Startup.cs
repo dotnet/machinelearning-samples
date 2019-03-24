a07ad9e67771fa3ed62e65273bfd035efe4c83a0
Cesar De la Torre Llorente <cesardl@microsoft.com>
2019-03-24 14:19:13 -0700

    First version of MLModelEngine (Optimized with Obj Pooling)

---
+++ samples/csharp/end-to-end-apps/ScalableMLModelOnWebAPI/src/Scalable.WebAPI/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scalable.Model.DataModels;
+using Scalable.Model.Engine;
+using System.IO;
+
+namespace Scalable.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the MLModelEngine with ObjPooling implementation as 'Singleton'
+            // in the IoC container for DI
+            //
+            services.AddSingleton<MLModelEngine<SampleObservation, SamplePrediction>>((ctx) =>
+                                  {
+                                      string modelFilePathName = GetAbsolutePath(Configuration["MLModel:MLModelFilePath"]);
+                                      return new MLModelEngine<SampleObservation, SamplePrediction>(modelFilePathName);
+                                  });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+    }
+}
