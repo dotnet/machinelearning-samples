ca5344c95b72e6d68e3f665fda44f5fb64729aca
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-03-26 22:27:41 -0700

    Added images for spike detection and change point detection. Changed the method names Changed ReadMe file.

--- samples/csharp/getting-started/SpikeDetection_ShampooSales/ShampooSales/ShampooSalesConsoleApp/Program.cs
+++ samples/csharp/getting-started/SpikeDetection_ShampooSales/ShampooSales/ShampooSalesConsoleApp/Program.cs
@@ -29,12 +29,17 @@ namespace ShampooSales
             IDataView dataView = mlcontext.Data.LoadFromTextFile<ShampooSalesData>(path: DatasetPath, hasHeader: true, separatorChar: ',');
 
             //To detech temporay changes in the pattern
-            Shamppo_IIDSpikeDetection(mlcontext,size,dataView);
+            DetectSpike(mlcontext,size,dataView);
 
             //To detect persistent change in the pattern
-            Shamppo_IIDChangepointDetection(mlcontext, size, dataView);
+            DetectChangepoint(mlcontext, size, dataView);
+
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+
+            Console.ReadLine();
         }
-        static void Shamppo_IIDSpikeDetection(MLContext mlcontext,int size,IDataView dataView)
+
+        static void DetectSpike(MLContext mlcontext,int size,IDataView dataView)
         {
            Console.WriteLine("Detect temporary changes in pattern");
 
@@ -42,7 +47,7 @@ namespace ShampooSales
             var trainingPipeLine = mlcontext.Transforms.DetectIidSpike(outputColumnName: nameof(ShampooSalesPrediction.Prediction), inputColumnName: nameof(ShampooSalesData.numSales),confidence: 95, pvalueHistoryLength: size / 4);
 
             //STEP 3:Train the model by fitting the dataview
-            Console.WriteLine("=============== Training the model ===============");
+            Console.WriteLine("=============== Training the model using Spike Detection algorithm ===============");
             ITransformer trainedModel = trainingPipeLine.Fit(dataView);
             Console.WriteLine("=============== End of training process ===============");
 
@@ -64,40 +69,37 @@ namespace ShampooSales
             Console.WriteLine("");
         }
 
-        static void Shamppo_IIDChangepointDetection(MLContext mlcontext, int size, IDataView dataView)
+        static void DetectChangepoint(MLContext mlcontext, int size, IDataView dataView)
         {
-            Console.WriteLine("Detect Persistent changes in pattern");
+          Console.WriteLine("Detect Persistent changes in pattern");
 
-            //STEP 2: Set the training algorithm    
-            var trainingPipeLine = mlcontext.Transforms.DetectIidChangePoint(outputColumnName: nameof(ShampooSalesPrediction.Prediction), inputColumnName: nameof(ShampooSalesData.numSales), confidence: 95, changeHistoryLength: size / 4);
+          //STEP 2: Set the training algorithm    
+          var trainingPipeLine = mlcontext.Transforms.DetectIidChangePoint(outputColumnName: nameof(ShampooSalesPrediction.Prediction), inputColumnName: nameof(ShampooSalesData.numSales), confidence: 95, changeHistoryLength: size / 4);
 
-            //STEP 3:Train the model by fitting the dataview
-            Console.WriteLine("=============== Training the model ===============");
-            ITransformer trainedModel = trainingPipeLine.Fit(dataView);
-            Console.WriteLine("=============== End of training process ===============");
+          //STEP 3:Train the model by fitting the dataview
+          Console.WriteLine("=============== Training the model Using Change Point Detection Algorithm===============");
+          ITransformer trainedModel = trainingPipeLine.Fit(dataView);
+          Console.WriteLine("=============== End of training process ===============");
 
-            //Apply data transformation to create predictions.
-            IDataView transformedData = trainedModel.Transform(dataView);
-            var predictions = mlcontext.Data.CreateEnumerable<ShampooSalesPrediction>(transformedData, reuseRowObject: false);
-                         
-            Console.WriteLine($"{nameof(ShampooSalesPrediction.Prediction)} column obtained post-transformation.");
-            Console.WriteLine("Alert\tScore\tP-Value\tMartingale value");
+          //Apply data transformation to create predictions.
+          IDataView transformedData = trainedModel.Transform(dataView);
+          var predictions = mlcontext.Data.CreateEnumerable<ShampooSalesPrediction>(transformedData, reuseRowObject: false);
+                       
+          Console.WriteLine($"{nameof(ShampooSalesPrediction.Prediction)} column obtained post-transformation.");
+          Console.WriteLine("Alert\tScore\tP-Value\tMartingale value");
             
-           foreach(var p in predictions)
-            {
-                if (p.Prediction[0] == 1)
-                {
-                    Console.WriteLine("{0}\t{1:0.00}\t{2:0.00}\t{3:0.00}  <-- alert is on, predicted changepoint", p.Prediction[0], p.Prediction[1], p.Prediction[2], p.Prediction[3]);
-                }
-                else
-                { 
-                    Console.WriteLine("{0}\t{1:0.00}\t{2:0.00}\t{3:0.00}",  p.Prediction[0], p.Prediction[1], p.Prediction[2], p.Prediction[3]);                  
-                }            
-            }
+          foreach(var p in predictions)
+          {
+             if (p.Prediction[0] == 1)
+             {
+                 Console.WriteLine("{0}\t{1:0.00}\t{2:0.00}\t{3:0.00}  <-- alert is on, predicted changepoint", p.Prediction[0], p.Prediction[1], p.Prediction[2], p.Prediction[3]);
+             }
+             else
+             { 
+                 Console.WriteLine("{0}\t{1:0.00}\t{2:0.00}\t{3:0.00}",  p.Prediction[0], p.Prediction[1], p.Prediction[2], p.Prediction[3]);                  
+             }            
+          }
             Console.WriteLine("");
-            Console.WriteLine("=============== End of process, hit any key to finish ===============");
-
-            Console.ReadLine();
         }
 
         public static string GetAbsolutePath(string relativePath)
