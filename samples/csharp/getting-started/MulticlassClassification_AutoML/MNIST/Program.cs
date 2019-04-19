e71a443aea2ed186904bf3a75b91dad4a0818960
Sabina Cartacio <scart039@fiu.edu>
2019-04-19 15:57:40 -0700

    Multiclass AutoML Sample

---
+++ samples/csharp/getting-started/MulticlassClassification_AutoML/MNIST/Program.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Auto;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using mnist.DataStructures;
+
+namespace mnist
+{
+    class Program
+    {
+        private static string BaseDatasetsRelativePath = @"../../../Data";
+        private static string TrianDataRealtivePath = $"{BaseDatasetsRelativePath}/optdigits-train.csv";
+        private static string TestDataRealtivePath = $"{BaseDatasetsRelativePath}/optdigits-val.csv";
+
+        private static string TrainDataPath = GetAbsolutePath(TrianDataRealtivePath);
+        private static string TestDataPath = GetAbsolutePath(TestDataRealtivePath);
+
+        private static string BaseModelsRelativePath = @"../../../MLModels";
+        private static string ModelRelativePath = $"{BaseModelsRelativePath}/Model.zip";
+
+        private static string ModelPath = GetAbsolutePath(ModelRelativePath);
+        private static uint ExperimentTime = 60;
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            Train(mlContext);
+            TestSomePredictions(mlContext);
+
+            Console.WriteLine("Hit any key to finish the app");
+            Console.ReadKey();
+        }
+
+        public static void Train(MLContext mlContext)
+        {
+            try
+            {
+                // STEP 1: Common data loading configuration
+                var trainData = mlContext.Data.LoadFromTextFile(path: TrainDataPath,
+                        columns : new[] 
+                        {
+                            new TextLoader.Column(nameof(InputData.PixelValues), DataKind.Single, 0, 63),
+                            new TextLoader.Column("Number", DataKind.Single, 64)
+                        },
+                        hasHeader : false,
+                        separatorChar : ','
+                        );
+
+                
+                var testData = mlContext.Data.LoadFromTextFile(path: TestDataPath,
+                        columns: new[]
+                        {
+                            new TextLoader.Column(nameof(InputData.PixelValues), DataKind.Single, 0, 63),
+                            new TextLoader.Column("Number", DataKind.Single, 64)
+                        },
+                        hasHeader: false,
+                        separatorChar: ','
+                        );
+
+                
+                // STEP 3: Auto featurize, auto train and auto hyperparameter tune
+                Console.WriteLine("=============== Training the model ===============");
+                Console.WriteLine($"Running AutoML multiclass classification experiment for {ExperimentTime} seconds...");
+                IEnumerable<RunDetails<MulticlassClassificationMetrics>> runDetails = mlContext.Auto()
+                                                                                .CreateMulticlassClassificationExperiment(ExperimentTime)
+                                                                                .Execute(trainData);
+
+                Console.WriteLine("===== Evaluating Model's accuracy with Test data =====");
+                RunDetails<MulticlassClassificationMetrics> best = runDetails.Best();
+                ITransformer trainedModel = best.Model;
+                var predictions = trainedModel.Transform(testData);
+                var metrics = mlContext.MulticlassClassification.Evaluate(data:predictions, labelColumnName:"Number", scoreColumnName:"Score");
+
+                //Common.ConsoleHelper.PrintMultiClassClassificationMetrics(trainer.ToString(), metrics);
+
+                mlContext.Model.Save(trainedModel, trainData.Schema, ModelPath);
+
+                Console.WriteLine("The model is saved to {0}", ModelPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //return null;
+            }
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+
+        private static void TestSomePredictions(MLContext mlContext)
+        {
+            ITransformer trainedModel = mlContext.Model.Load(ModelPath, out var modelInputSchema);
+
+            // Create prediction engine related to the loaded trained model
+            var predEngine = mlContext.Model.CreatePredictionEngine<InputData, OutPutData>(trainedModel);
+
+            //InputData data1 = SampleMNISTData.MNIST1;
+            var resultprediction1 = predEngine.Predict(SampleMNISTData.MNIST1);
+
+            Console.WriteLine($"Actual: 7     Predicted probability:       zero:  {resultprediction1.Score[0]:0.####}");
+            Console.WriteLine($"                                           One :  {resultprediction1.Score[1]:0.####}");
+            Console.WriteLine($"                                           two:   {resultprediction1.Score[2]:0.####}");
+            Console.WriteLine($"                                           three: {resultprediction1.Score[3]:0.####}");
+            Console.WriteLine($"                                           four:  {resultprediction1.Score[4]:0.####}");
+            Console.WriteLine($"                                           five:  {resultprediction1.Score[5]:0.####}");
+            Console.WriteLine($"                                           six:   {resultprediction1.Score[6]:0.####}");
+            Console.WriteLine($"                                           seven: {resultprediction1.Score[7]:0.####}");
+            Console.WriteLine($"                                           eight: {resultprediction1.Score[8]:0.####}");
+            Console.WriteLine($"                                           nine:  {resultprediction1.Score[9]:0.####}");
+            Console.WriteLine();
+                       
+            var resultprediction2 = predEngine.Predict(SampleMNISTData.MNIST2);
+
+            Console.WriteLine($"Actual: 1     Predicted probability:       zero:  {resultprediction2.Score[0]:0.####}");
+            Console.WriteLine($"                                           One :  {resultprediction2.Score[1]:0.####}");
+            Console.WriteLine($"                                           two:   {resultprediction2.Score[2]:0.####}");
+            Console.WriteLine($"                                           three: {resultprediction2.Score[3]:0.####}");
+            Console.WriteLine($"                                           four:  {resultprediction2.Score[4]:0.####}");
+            Console.WriteLine($"                                           five:  {resultprediction2.Score[5]:0.####}");
+            Console.WriteLine($"                                           six:   {resultprediction2.Score[6]:0.####}");
+            Console.WriteLine($"                                           seven: {resultprediction2.Score[7]:0.####}");
+            Console.WriteLine($"                                           eight: {resultprediction2.Score[8]:0.####}");
+            Console.WriteLine($"                                           nine:  {resultprediction2.Score[9]:0.####}");
+            Console.WriteLine();
+
+            var resultprediction3 = predEngine.Predict(SampleMNISTData.MNIST3);
+
+            Console.WriteLine($"Actual: 9     Predicted probability:       zero:  {resultprediction3.Score[0]:0.####}");
+            Console.WriteLine($"                                           One :  {resultprediction3.Score[1]:0.####}");
+            Console.WriteLine($"                                           two:   {resultprediction3.Score[2]:0.####}");
+            Console.WriteLine($"                                           three: {resultprediction3.Score[3]:0.####}");
+            Console.WriteLine($"                                           four:  {resultprediction3.Score[4]:0.####}");
+            Console.WriteLine($"                                           five:  {resultprediction3.Score[5]:0.####}");
+            Console.WriteLine($"                                           six:   {resultprediction3.Score[6]:0.####}");
+            Console.WriteLine($"                                           seven: {resultprediction3.Score[7]:0.####}");
+            Console.WriteLine($"                                           eight: {resultprediction3.Score[8]:0.####}");
+            Console.WriteLine($"                                           nine:  {resultprediction3.Score[9]:0.####}");
+            Console.WriteLine();
+        }
+    }
+}
