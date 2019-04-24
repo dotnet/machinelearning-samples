90e5685bc8421a3de34900e9fabd18832d972268
prathyusha12345 <v-prkor@microsoft.com>
2019-04-24 15:28:18 -0700

    Rename shampoo sales to  AnomalyDetection_sales (#387)
    * Renamed shampoo sales to generic product sales so that this sample will be suitable for all kinds of product sales data
    
    * Renamed the app
    
    * Renamed project names.
    Moved data and Models folders to commom location of both winfoms and console projects.
    
    * Loaded the changed projects in solution file.
    
    * fixed the path
    
    * Fixed path
    
    * fixed path

---
+++ samples/csharp/end-to-end-apps/AnomalyDetection-Sales/SpikeDetection.WinForms/DataStructures/ProductSalesData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace SpikeDetection.WinForms
+{
+    public class ProductSalesData
+    {
+        [LoadColumn(0)]
+        public string Month;
+
+        [LoadColumn(1)]
+        public float numSales;
+    }
+}
