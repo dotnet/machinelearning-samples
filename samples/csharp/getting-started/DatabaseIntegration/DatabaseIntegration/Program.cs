ba8eb4e74bd147890d34be08964d0467e4ce8a4d
Scott Inglis <msinglft@gmail.com>
2019-04-24 12:22:10 -0700

    - Addressing feedback, updating the README.md.

--- samples/csharp/getting-started/DatabaseIntegration/DatabaseIntegration/Program.cs
+++ samples/csharp/getting-started/DatabaseIntegration/DatabaseIntegration/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.ML;
+using Microsoft.EntityFrameworkCore;
 using Common;
 using System;
 using System.IO;
@@ -10,11 +11,18 @@ namespace DatabaseIntegration
 {
     public class Program
     {
-        private static string Download(string baseGitPath, string dataFile)
+
+        /// <summary>
+        /// Helper function to download a file given a base uri
+        /// </summary>
+        /// <param name="uriPath">The uri of where to download the file.</param>
+        /// <param name="dataFile">The location of where to save the downloaded file.</param>
+        /// <returns></returns>
+        private static string Download(string uriPath, string dataFile)
         {
             using (WebClient client = new WebClient())
             {
-                client.DownloadFile(new Uri($"{baseGitPath}"), dataFile);
+                client.DownloadFile(new Uri($"{uriPath}"), dataFile);
             }
 
             return dataFile;
@@ -23,13 +31,19 @@ namespace DatabaseIntegration
         public static string DownloadAdultDataset()
             => Download("https://raw.githubusercontent.com/dotnet/machinelearning/244a8c2ac832657af282aa312d568211698790aa/test/data/adult.train", "adult.txt");
 
+        /// <summary>
+        /// Populates the database with the AdultCensus dataset.
+        /// </summary>
         public static void Seeding()
         {
             string adultDataset = DownloadAdultDataset();
             using (var db = new AdultCensusContext())
             {
+                // Ensure that we have a clean database to start with.
                 db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
+
+                // Parse the downloaded file
                 var data = File.ReadLines(adultDataset)
                     .Skip(1) // Skip the header row
                     .Select(l => l.Split(','))
@@ -39,7 +53,6 @@ namespace DatabaseIntegration
                             Age = int.Parse(row[0]),
                             Workclass = row[1],
                             Education = row[3],
-                            //EducationNum = int.Parse(data[4]),
                             MaritalStatus = row[5],
                             Occupation = row[6],
                             Relationship = row[7],
@@ -51,7 +64,10 @@ namespace DatabaseIntegration
                             NativeCountry = row[13],
                             Label = (int.Parse(row[14]) == 1) ? true : false
                     });
+
+                // Add the data into the database
                 db.AdultCensus.AddRange(data);
+
                 var count = db.SaveChanges();
                 Console.WriteLine($"Save results {count}");
             }
@@ -59,13 +75,21 @@ namespace DatabaseIntegration
 
         public static void Main()
         {
+            // Seed the database with the dataset.
             Seeding();
+
             using(var db = new AdultCensusContext())
             {
+                // Set the query tracking behavior to be NoTracking, this is particularly useful because
+                // our scenarios our read-only with the data. Not having tracking will  
+                db.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
                 var mlContext =  new MLContext();
-                
-                // NOTE: For training, we are sorting the data by the AdultCensusId, which is an auto-generated id. ML.Net requires
-                // that the training data is processed in the same order to produce consistent results with the model.
+               
+                // Query our training data from the database. This query is selecting everything from the AdultCensus table. The
+                // result is then loaded by ML.Net through the LoadFromEnumerable. LoadFromEnumerable returns an IDataView which
+                // can be consumed by an ML.Net pipeline.
+                // NOTE: For training, ML.Net requires that the training data is processed in the same order to produce consistent results.
+                // Therefore we are sorting the data by the AdultCensusId, which is an auto-generated id.
                 var dataView = mlContext.Data.LoadFromEnumerable(db.AdultCensus.OrderBy(x=>x.AdultCensusId).ToList<AdultCensus>());
                 var pipeline = mlContext.Transforms.Categorical.OneHotEncoding(new [] {
                         new InputOutputColumnPair("MsOHE", "MaritalStatus"), 
@@ -79,10 +103,16 @@ namespace DatabaseIntegration
 
                 Console.WriteLine("Training model...");
                 var model = pipeline.Fit(dataView);
-                var test = db.AdultCensus.Take(2000).ToList<AdultCensus>();
 
+                Console.WriteLine("Predicting...");
+                // Now that the model is trained, we want to test it's prediction results, which is done by using a test dataset
+                // and transforming it by the trained model.
+                // To retrieve our test data, this issues a second query, retrieving the first 2000 records, then converts to an IDataView
+                // in order to feed into the ML.Net pipeline.
                 var testDataView = mlContext.Data.LoadFromEnumerable(db.AdultCensus.Take(2000).ToList<AdultCensus>());
                 var predictions = model.Transform(testDataView);
+
+                // Now that we have the predictions, calculate th metrics of those predictions and output the results.
                 var metrics = mlContext.BinaryClassification.Evaluate(predictions);
                 ConsoleHelper.PrintBinaryClassificationMetrics("Database Example", metrics);
                 ConsoleHelper.ConsolePressAnyKey();
--- samples/csharp/getting-started/DatabaseIntegration/README.md
+++ samples/csharp/getting-started/DatabaseIntegration/README.md
@@ -1,20 +1,18 @@
 # Using a database as a data source
-This sample demonstrates how to use a database as a data source for an ML.Net pipeline using the IDataView interface. The data can then be used for training and testing a model. In order
-to support mulitple databass the Entity Framework is used as part of this sample.
+This sample demonstrates how to use a database as a data source for an ML.Net pipeline. As ML.Net does not have native support for a database, this sample shows how data can be accessed using an IEnumerable. Since the database is treated as any other datasource, it is possible to query the database and use the resulting data for training and prediction scenarios.
 
 ## Problem
-
-In scenarios where the dataset lives in a database, the dataset is likely large and associated but not live within a single table. In addition, having to massage the data to be consumeable into an machine learning pipeline can be an expensive and tedious chore.
-
+Enterprise users have a need to use their existing data set that is in their company's database to train and predict with ML.Net. They need support to leverage their existing relational table schema, ability to read from the database directly, and to be aware of memory limitations as the data is being consumed.
 
 ## Solution
-This sample shows how to use the Entity Framework to connect to a database and leverage the existing schema allowing to query for the data that is needed to train and make this available to ML.Net through the IDataView interface.
+This sample shows how to use the Entity Framework Core to connect to a database, query  and feed the resulting data into an ML.Net pipeline.
 
-This sample uses SQLite to help demonstrate the database integration, but any database that is supported by the Entity Framwork can be used. This sample also uses ML.Net's ability to convert an IEnumerable to and IDataView -- as the Entity Framework will expose the results of a query as an IEnumberable. To get started the following commands need to run to setup the database. To create the database, the dot net migrations commands are used.
+This sample uses SQLite to help demonstrate the database integration, but any database that is supported by the Entity Framwork Core can be used. As ML.Net can  create an IDataView from an IEnumerable, this sample will use the IEnumerable that is returned from a query to feed the data into the ML.Net pipeline. To prevent the Entity Framework Core from loading all the data in from a result, a no tracking query is used. 
 
+To get started the following dot net migration commands need to run to setup the database. 
 
 ### 1. Create the database
-From the command line in the directory where the DatabaseIntegration.csproj file is located, type the following cmomands:
+From the command line in the directory where the DatabaseIntegration.csproj file is located, type the following commands:
 ```
 dotnet ef migrations add InitialCreate
 dotnet ef database update
@@ -22,7 +20,14 @@ dotnet ef database update
 ### 2. Open the solution, build and run the code
 The sample will do the following:
 - Downloads a sample dataset
-- Upload the dataset into the database
+- Seeds the data into the database
 - Query the database for the dataset
 - Converts the IEnumerable to IDataView
 - Trains a LightGBM Binary Classification model 
+- Queries the database for a test dataset
+- Runs predictions
+- Evaluates the prediction metrics
+
+Note: The working directory will need to be set to the directory where DatabaseIntegration.csproj is located, otherwise the database will not be seeded.
+
+
