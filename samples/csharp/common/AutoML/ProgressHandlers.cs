5831bdd9bea8e42e1d3e4967486653a5df1abe4c
Cesar De la Torre <cesardl@microsoft.com>
2019-05-04 16:05:38 -0700

    Added AutoML API C# samples (#410)

---
+++ samples/csharp/common/AutoML/ProgressHandlers.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+
+namespace Common
+{
+    /// <summary>
+    /// Progress handler that AutoML will invoke after each model it produces and evaluates.
+    /// </summary>
+    public class BinaryExperimentProgressHandler : IProgress<RunDetail<BinaryClassificationMetrics>>
+    {
+        private int _iterationIndex;
+
+        public void Report(RunDetail<BinaryClassificationMetrics> iterationResult)
+        {
+            if (_iterationIndex++ == 0)
+            {
+                ConsoleHelper.PrintBinaryClassificationMetricsHeader();
+            }
+
+            if (iterationResult.Exception != null)
+            {
+                ConsoleHelper.PrintIterationException(iterationResult.Exception);
+            }
+            else
+            {
+                ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName,
+                    iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Progress handler that AutoML will invoke after each model it produces and evaluates.
+    /// </summary>
+    public class MulticlassExperimentProgressHandler : IProgress<RunDetail<MulticlassClassificationMetrics>>
+    {
+        private int _iterationIndex;
+
+        public void Report(RunDetail<MulticlassClassificationMetrics> iterationResult)
+        {
+            if (_iterationIndex++ == 0)
+            {
+                ConsoleHelper.PrintMulticlassClassificationMetricsHeader();
+            }
+
+            if (iterationResult.Exception != null)
+            {
+                ConsoleHelper.PrintIterationException(iterationResult.Exception);
+            }
+            else
+            {
+                ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName,
+                    iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Progress handler that AutoML will invoke after each model it produces and evaluates.
+    /// </summary>
+    public class RegressionExperimentProgressHandler : IProgress<RunDetail<RegressionMetrics>>
+    {
+        private int _iterationIndex;
+
+        public void Report(RunDetail<RegressionMetrics> iterationResult)
+        {
+            if (_iterationIndex++ == 0)
+            {
+                ConsoleHelper.PrintRegressionMetricsHeader();
+            }
+
+            if (iterationResult.Exception != null)
+            {
+                ConsoleHelper.PrintIterationException(iterationResult.Exception);
+            }
+            else
+            {
+                ConsoleHelper.PrintIterationMetrics(_iterationIndex, iterationResult.TrainerName,
+                    iterationResult.ValidationMetrics, iterationResult.RuntimeInSeconds);
+            }
+        }
+    }
+}
