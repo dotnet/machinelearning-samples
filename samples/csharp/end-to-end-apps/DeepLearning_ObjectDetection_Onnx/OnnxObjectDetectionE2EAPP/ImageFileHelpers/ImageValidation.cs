20519fd4d03002fd639244de1ed86e8aedba9e00
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-05-10 12:29:47 -0700

    Added licensed images

--- samples/csharp/end-to-end-apps/DeepLearning_ObjectDetection_Onnx/OnnxObjectDetectionE2EAPP/ImageFileHelpers/ImageValidation.cs
+++ samples/csharp/end-to-end-apps/DeepLearning_ObjectDetection_Onnx/OnnxObjectDetectionE2EAPP/ImageFileHelpers/ImageValidation.cs
@@ -32,6 +32,8 @@ namespace OnnxObjectDetectionE2EAPP.Infrastructure
             var tiff2 = new byte[] { 77, 77, 42 };         // TIFF
             var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
             var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
+            var jpg1 = new byte[] { 255, 216, 255, 219 };
+            var jpg2 = new byte[] { 255, 216, 255, 226 };
 
             if (bmp.SequenceEqual(bytes.Take(bmp.Length)))
                 return ImageFormat.bmp;
@@ -48,7 +50,7 @@ namespace OnnxObjectDetectionE2EAPP.Infrastructure
             if (tiff2.SequenceEqual(bytes.Take(tiff2.Length)))
                 return ImageFormat.tiff;
 
-            if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)))
+            if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)) || jpg1.SequenceEqual(bytes.Take(jpg1.Length)) || jpg2.SequenceEqual(bytes.Take(jpg2.Length)))
                 return ImageFormat.jpeg;
 
             if (jpeg2.SequenceEqual(bytes.Take(jpeg2.Length)))
