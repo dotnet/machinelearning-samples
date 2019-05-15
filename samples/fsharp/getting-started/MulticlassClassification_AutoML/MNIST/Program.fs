28383fee08f07f6ea1489cc33b365ebc3cf34bb5
kevmal <kevin@xaai.ch>
2019-05-15 12:48:26 -0600

    add prefeaturized so Score item order is known

--- samples/fsharp/getting-started/MulticlassClassification_AutoML/MNIST/Program.fs
+++ samples/fsharp/getting-started/MulticlassClassification_AutoML/MNIST/Program.fs
@@ -6,6 +6,7 @@ open Microsoft.ML
 open Microsoft.ML.AutoML
 open Microsoft.ML.Data
 open Common
+open System.Collections.Generic
 
 [<CLIMutable>]
 type InputData =
@@ -32,10 +33,6 @@ let sampleData =
         7, {
             Number = 0.f
             PixelValues = [|0.f;0.f;1.f;8.f;15.f;10.f;0.f;0.f;0.f;3.f;13.f;15.f;14.f;14.f;0.f;0.f;0.f;5.f;10.f;0.f;10.f;12.f;0.f;0.f;0.f;0.f;3.f;5.f;15.f;10.f;2.f;0.f;0.f;0.f;16.f;16.f;16.f;16.f;12.f;0.f;0.f;1.f;8.f;12.f;14.f;8.f;3.f;0.f;0.f;0.f;0.f;10.f;13.f;0.f;0.f;0.f;0.f;0.f;0.f;11.f;9.f;0.f;0.f;0.f|]
-        }
-        9, {
-            Number = 0.f
-            PixelValues = [|0.f;0.f;6.f;14.f;4.f;0.f;0.f;0.f;0.f;0.f;11.f;16.f;10.f;0.f;0.f;0.f;0.f;0.f;8.f;14.f;16.f;2.f;0.f;0.f;0.f;0.f;1.f;12.f;12.f;11.f;0.f;0.f;0.f;0.f;0.f;0.f;0.f;11.f;3.f;0.f;0.f;0.f;0.f;0.f;0.f;5.f;11.f;0.f;0.f;0.f;1.f;4.f;4.f;7.f;16.f;2.f;0.f;0.f;7.f;16.f;16.f;13.f;11.f;1.f|]
         }
     |]
 
@@ -79,7 +76,8 @@ try
     // STEP 3: Run an AutoML multiclass classification experiment
     ConsoleHelper.consoleWriteHeader "=============== Running AutoML experiment ==============="
     printfn "Running AutoML multiclass classification experiment for %d seconds..." experimentTimeInSeconds
-    let experimentResult = mlContext.Auto().CreateMulticlassClassificationExperiment(experimentTimeInSeconds).Execute(trainData, "Number", progressHandler = progressHandler)
+    let preFeaturizer = mlContext.Transforms.Conversion.MapValueToKey("Number", "Number", keyOrdinality = Transforms.ValueToKeyMappingEstimator.KeyOrdinality.ByValue) |> ConsoleHelper.downcastPipeline
+    let experimentResult = mlContext.Auto().CreateMulticlassClassificationExperiment(experimentTimeInSeconds).Execute(trainData, "Number", progressHandler = progressHandler, preFeaturizer = preFeaturizer)
 
     // Print top models found by AutoML
     printfn ""
