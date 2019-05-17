3a165dc308ca823bfcfa4f533cb5f54ebdc7f977
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-05-16 17:59:03 -0700

    Added Integration package for Movie Recommender E2e sample.

---
+++ samples/csharp/end-to-end-apps/Recommendation-MovieRecommender/MovieRecommender/movierecommender/DataStructures/MovieRatingPrediction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRecommender.DataStructures
+{
+    public class MovieRatingPrediction
+    {
+        public bool Label;
+
+        public float Score;
+    }
+}
