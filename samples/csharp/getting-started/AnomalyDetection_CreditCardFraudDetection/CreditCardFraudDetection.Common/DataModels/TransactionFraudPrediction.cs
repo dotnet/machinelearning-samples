c3c950e6561d8191b683566377390630f4edab70
Colby Williams <ColbyLWilliams@gmail.com>
2019-05-28 12:34:35 -0400

    Copy DataModels from BinaryClassification_CreditCardFraudDetection

---
+++ samples/csharp/getting-started/AnomalyDetection_CreditCardFraudDetection/CreditCardFraudDetection.Common/DataModels/TransactionFraudPrediction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ML.Data;
+//using static Microsoft.ML.Runtime.Data.RoleMappedSchema;
+
+namespace CreditCardFraudDetection.Common.DataModels
+{
+    public class TransactionFraudPrediction : IModelEntity
+    {
+        public bool Label;
+        public bool PredictedLabel;
+        public float Score;
+        public float Probability;
+
+        public void PrintToConsole()
+        {
+            Console.WriteLine($"Predicted Label: {PredictedLabel}");
+            Console.WriteLine($"Probability: {Probability}  ({Score})");
+        }
+    }
+}
