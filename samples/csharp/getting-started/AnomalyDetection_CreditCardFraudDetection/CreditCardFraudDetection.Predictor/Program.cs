44b264bfc252e4cb5f7d6049083edcd7ee7454de
Colby Williams <ColbyLWilliams@gmail.com>
2019-05-28 11:32:18 -0400

    empty console app creation

---
+++ samples/csharp/getting-started/AnomalyDetection_CreditCardFraudDetection/CreditCardFraudDetection.Predictor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditCardFraudDetection.Predictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ samples/csharp/getting-started/AnomalyDetection_CreditCardFraudDetection/CreditCardFraudDetection.Trainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditCardFraudDetection.Trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
