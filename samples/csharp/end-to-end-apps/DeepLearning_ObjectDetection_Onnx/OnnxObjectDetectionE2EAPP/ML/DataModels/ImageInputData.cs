dee0dced08e6513f23a90ace00bda1f8b670945f
Prathyusha Korrapati <v-prkor@microsoft.com>
2019-06-07 10:56:23 -0700

    Added attribute

--- samples/csharp/end-to-end-apps/DeepLearning_ObjectDetection_Onnx/OnnxObjectDetectionE2EAPP/ML/DataModels/ImageInputData.cs
+++ samples/csharp/end-to-end-apps/DeepLearning_ObjectDetection_Onnx/OnnxObjectDetectionE2EAPP/ML/DataModels/ImageInputData.cs
@@ -1,12 +1,11 @@
-﻿using System.Drawing;
-using Microsoft.ML.Transforms.Image.
+﻿using Microsoft.ML.Transforms.Image;
+using System.Drawing;
 
 namespace OnnxObjectDetectionE2EAPP
 {
     public class ImageInputData
     {
-
-        [ImageType(227, 227)]
+        [ImageType(416, 416)]
         public Bitmap Image { get; set; }
     }
 }
