96706dcf3a7bd9838d5ae4fcbbba6596bc72b7b5
Colby Williams <ColbyLWilliams@gmail.com>
2019-08-08 08:19:04 -0400

    added toolkit

---
+++ samples/csharp/end-to-end-apps/ObjectDetection_Onnx/OnnxObjectDetectionStreamingApp/VideoCapture.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFMediaKit.DirectShow.Controls;
+
+namespace OnnxObjectDetectionStreamingApp
+{
+    /// <summary>
+    /// Interaction logic for VideoCapture.xaml
+    /// </summary>
+    public partial class VideoCapture : Page
+    {
+        public VideoCapture()
+        {
+            InitializeComponent();
+        }
+    }
+}
