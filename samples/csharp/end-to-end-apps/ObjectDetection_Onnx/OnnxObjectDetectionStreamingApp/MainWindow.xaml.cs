4d3dd26b621fcb516a3656eea07bb4dc86edfa64
Colby Williams <ColbyLWilliams@gmail.com>
2019-08-08 10:24:10 -0400

    view live webcam stream

--- samples/csharp/end-to-end-apps/ObjectDetection_Onnx/OnnxObjectDetectionStreamingApp/MainWindow.xaml.cs
+++ samples/csharp/end-to-end-apps/ObjectDetection_Onnx/OnnxObjectDetectionStreamingApp/MainWindow.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using OpenCvSharp;
-using OpenCvSharp.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OnnxObjectDetectionStreamingApp
 {
@@ -25,10 +11,11 @@ namespace OnnxObjectDetectionStreamingApp
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        VideoCapture capture;
         Mat frame;
-        private Thread camera;
+        VideoCapture capture;
         bool isCameraRunning = false;
+
+        private Thread camera;
 
         public MainWindow()
         {
@@ -56,25 +43,27 @@ namespace OnnxObjectDetectionStreamingApp
                 while (isCameraRunning)
                 {
                     capture.Read(frame);
+                    
+                    // TODO: Kill after user closes the app window or it throws an exception here
 
-                    Application.Current.Dispatcher.Invoke((Action)(() =>
+                    Application.Current.Dispatcher.Invoke(() =>
                     {
-                        using (MemoryStream memoryStream = frame.ToMemoryStream())
-                        {
-                            var imageSource = new BitmapImage();
-                            imageSource.BeginInit();
-                            imageSource.StreamSource = memoryStream;
-                            imageSource.EndInit();
+                        using MemoryStream memoryStream = frame.ToMemoryStream();
 
-                            // Attempting to show the image either in a MediaElement or Image control - neither seem to be working currently - need to investigate further
-                            WebCamImage.Source = imageSource;
-                            WebCamMedia.Source = imageSource.UriSource; //This is always null - not sure how else to get the Uri
+                        var imageSource = new BitmapImage();
 
-                            // This works (change the path to your machine) - so, we are getting frames from the web cam
-                            var testImage = BitmapConverter.ToBitmap(frame);
-                            testImage.Save(@"C:\Users\nicolela\Documents\TestImages\TestImage.png");
-                        }
-                    }));
+                        imageSource.BeginInit();
+                        imageSource.CacheOption = BitmapCacheOption.OnLoad;
+                        imageSource.StreamSource = memoryStream;
+                        imageSource.EndInit();
+
+                        WebCamImage.Source = imageSource;
+
+                        // This works (change the path to your machine) - so, we are getting frames from the web cam
+                        // var testImage = BitmapConverter.ToBitmap(frame);
+                        // testImage.Save(@"C:\Users\nicolela\Documents\TestImages\TestImage.png");
+                        // testImage.Save(@"C:\Users\colbyw\Documents\TestImages\TestImage.png");
+                    });
                 }
             }
         }
