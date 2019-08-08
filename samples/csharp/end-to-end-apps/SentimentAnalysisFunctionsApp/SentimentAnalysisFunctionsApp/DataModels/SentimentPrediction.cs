916a90e6dafa9fd9ddc40bb2790c039c3c5ee978
Luis Quintanilla <luquinta@microsoft.com>
2019-08-08 15:50:30 -0400

    Adding Azure Functions Sentiment Analysis E2E App Sample

---
+++ samples/csharp/end-to-end-apps/SentimentAnalysisFunctionsApp/SentimentAnalysisFunctionsApp/DataModels/SentimentPrediction.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace SentimentAnalysisFunctionsApp.DataModels
+{
+    public class SentimentPrediction : SentimentData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
