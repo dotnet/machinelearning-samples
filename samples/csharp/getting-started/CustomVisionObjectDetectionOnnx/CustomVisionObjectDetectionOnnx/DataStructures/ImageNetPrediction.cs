54a82b428009cbd379ba5dde3f06bba7765c4b59
Luis Quintanilla <46974588+luisquintanilla@users.noreply.github.com>
2019-08-09 13:40:37 -0400

    WIP Azure Custom Vision ONNX Object Detection Sample (#603)
    * Custom Vision Object Detection Sample
    
    * Renaming folder

---
+++ samples/csharp/getting-started/CustomVisionObjectDetectionOnnx/CustomVisionObjectDetectionOnnx/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace CustomVisionObjectDetectionOnnx.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("model_outputs0")]
+        public float[] PredictedLabels;
+    }
+}
---
+++ samples/csharp/getting-started/CustomVisionObjectDetectionOnnx/CustomVisionObjectDetectionOnnx/YoloParser/DimensionsBase.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomVisionObjectDetectionOnnx.YoloParser
+{
+    public class DimensionsBase
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+    }
+}
