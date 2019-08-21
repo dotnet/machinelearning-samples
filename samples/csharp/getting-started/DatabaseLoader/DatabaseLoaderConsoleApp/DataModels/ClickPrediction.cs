1ae68a2c5c7603af2126002fc220ecacc99516af
CESARDELATORRE <cesardl@microsoft.com>
2019-08-21 23:29:41 +0000

    DatabaseLoader working against localdb attached from DB path file.

---
+++ samples/csharp/getting-started/DatabaseLoader/DatabaseLoaderConsoleApp/DataModels/ClickPrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLoaderConsoleApp.DataModels
+{
+    public class ClickPrediction
+    {
+        public bool PredictedLabel;
+
+        public float Score;
+    }
+}
