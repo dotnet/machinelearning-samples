bc306e5e52b272ec5cb001aeb5794ed18fa50035
Cesar De la Torre <cesardl@microsoft.com>
2019-09-02 17:19:47 +0000

    DatabaseLoader sample loading an IDataView from SQL Server localdb (#617)
    * DatabaseLoader sample - Baseline
    
    * DatabaseLoader working against localdb attached from DB path file.
    
    * Removed unneeded-old project file
    
    * Added tbd README.md file
    
    * Removed internal Nuget feeds from the nuget.config
    
    * Added a shorter name for the localdb database instead of based on the file path
    
    * Added Retries logic policy to the Connection String when using Azure SQL Database
    
    * Simplified so it doesnt use the ProviderFactory code but simple an SqlClientFactory.Instance
    
    * Updated to public Previews
    
    * Removed unneeded CsvParser reference
    
    * Comments update doe conn-strings

---
+++ samples/csharp/getting-started/DatabaseLoader/DatabaseLoaderConsoleApp/DataModels/UrlClick.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLoaderConsoleApp.DataModels
+{
+    public class UrlClick
+    {
+        public string Label { get; set; }
+        public string Feat01 { get; set; }
+        public string Feat02 { get; set; }
+        public string Feat03 { get; set; }
+        public string Feat04 { get; set; }
+        public string Feat05 { get; set; }
+        public string Feat06 { get; set; }
+        public string Feat07 { get; set; }
+        public string Feat08 { get; set; }
+        public string Feat09 { get; set; }
+        public string Feat10 { get; set; }
+        public string Feat11 { get; set; }
+        public string Feat12 { get; set; }
+        public string Feat13 { get; set; }
+        public string Cat14 { get; set; }
+        public string Cat15 { get; set; }
+        public string Cat16 { get; set; }
+        public string Cat17 { get; set; }
+        public string Cat18 { get; set; }
+        public string Cat19 { get; set; }
+        public string Cat20 { get; set; }
+        public string Cat21 { get; set; }
+        public string Cat22 { get; set; }
+        public string Cat23 { get; set; }
+        public string Cat24 { get; set; }
+        public string Cat25 { get; set; }
+        public string Cat26 { get; set; }
+        public string Cat27 { get; set; }
+        public string Cat28 { get; set; }
+        public string Cat29 { get; set; }
+        public string Cat30 { get; set; }
+        public string Cat31 { get; set; }
+        public string Cat32 { get; set; }
+        public string Cat33 { get; set; }
+        public string Cat34 { get; set; }
+        public string Cat35 { get; set; }
+        public string Cat36 { get; set; }
+        public string Cat37 { get; set; }
+        public string Cat38 { get; set; }
+        public string Cat39 { get; set; }
+    }
+}
