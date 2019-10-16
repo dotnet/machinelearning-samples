94c608fe8af46d2ea7e9417463481adca7158f81
Luis Quintanilla <luquinta@microsoft.com>
2019-10-16 17:13:08 -0400

    Initial Commit

---
+++ samples/modelbuilder/MulticlassClassification_RestaurantInspections/MulticlassClassification_RestaurantInspections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MulticlassClassification_RestaurantInspectionsML.Model;
+
+namespace MulticlassClassification_RestaurantInspections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create sample data
+            ModelInput input = new ModelInput
+            {
+                Violation_id = "80137_20161005_103149",
+                Inspection_type = "Routine - Unscheduled",
+                Violation_description = "Wiping cloths not clean or properly stored or inadequate sanitizer"
+            };
+
+            // Make prediction
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            // Print Prediction
+            Console.WriteLine($"Risk Category: {result.Prediction}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ samples/modelbuilder/MulticlassClassification_RestaurantInspections/README.md
@@ -0,0 +1,6 @@
+# Restaurant Violation Inspections
+
+
+| ML.NET version | Status                        | App Type    | Data type | Scenario            | ML Task                   | Algorithms                  |
+|----------------|-------------------------------|-------------|-----------|---------------------|---------------------------|-----------------------------|
+| v1.3.1           | Up-to-date | Console App | Single data sample | Issue Classification | Multiclass classification | Linear Classification |
