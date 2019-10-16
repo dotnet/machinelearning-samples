39aa95593cd57fae3d652be51d7e7e5a093d539f
Luis Quintanilla <luquinta@microsoft.com>
2019-10-16 16:33:15 -0400

    Initial commit

---
+++ samples/csharp/getting-started/DeepLearning_ImageClassification_Binary/DeepLearning_ImageClassification_Binary/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.ML;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Transforms;
+
+namespace DeepLearning_ImageClassification_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../"));
+            var assetsRelativePath = Path.Combine(projectDirectory, "assets");
+
+            MLContext mlContext = new MLContext();
+
+            IEnumerable<ModelInput> images = LoadImagesFromDirectory(folder: assetsRelativePath, useFolderNameAsLabel: true);
+
+            IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
+
+            IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
+
+            var preprocessingPipeline = mlContext.Transforms.Conversion.MapValueToKey(
+                    inputColumnName: "Label",
+                    outputColumnName: "LabelAsKey")
+                .Append(mlContext.Transforms.LoadImages(
+                    outputColumnName: "Image",
+                    imageFolder: assetsRelativePath,
+                    useImageType: false,
+                    inputColumnName: "ImagePath"));
+
+            IDataView preProcessedData = preprocessingPipeline
+                                .Fit(shuffledData)
+                                .Transform(shuffledData);
+
+            TrainTestData trainSplit = mlContext.Data.TrainTestSplit(data: preProcessedData, testFraction: 0.3);
+            TrainTestData validationTestSplit = mlContext.Data.TrainTestSplit(trainSplit.TestSet);
+
+            IDataView trainSet = trainSplit.TrainSet;
+            IDataView validationSet = validationTestSplit.TrainSet;
+            IDataView testSet = validationTestSplit.TestSet;
+
+            var trainingPipeline = mlContext.Model.ImageClassification(
+                    featuresColumnName: "Image",
+                    labelColumnName: "LabelAsKey",
+                    arch: ImageClassificationEstimator.Architecture.ResnetV2101,
+                    epoch: 100,
+                    batchSize: 20,
+                    testOnTrainSet: false,
+                    metricsCallback: (metrics) => Console.WriteLine(metrics),
+                    validationSet: validationSet,
+                    reuseTrainSetBottleneckCachedValues: true,
+                    reuseValidationSetBottleneckCachedValues: true,
+                    disableEarlyStopping: false)
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            ITransformer trainedModel = trainingPipeline.Fit(trainSet);
+
+            ClassifyImages(mlContext, testSet, trainedModel);
+        }
+
+        public static void ClassifyImages(MLContext mlContext, IDataView data, ITransformer trainedModel)
+        {
+            IDataView predictionData = trainedModel.Transform(data);
+
+            IEnumerable<ModelOutput> predictions = mlContext.Data.CreateEnumerable<ModelOutput>(predictionData, reuseRowObject: true).Take(10);
+
+            foreach (var prediction in predictions)
+            {
+                string imageName = Path.GetFileName(prediction.ImagePath);
+                Console.WriteLine($"Image: {imageName} | Actual Value: {prediction.Label} | Predicted Value: {prediction.PredictedLabel}");
+            }
+        }
+
+        public static IEnumerable<ModelInput> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+        {
+            var files = Directory.GetFiles(folder, "*",
+                searchOption: SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                if ((Path.GetExtension(file) != ".jpg") && (Path.GetExtension(file) != ".png"))
+                    continue;
+
+                var label = Path.GetFileName(file);
+
+                if (useFolderNameAsLabel)
+                    label = Directory.GetParent(file).Name;
+                else
+                {
+                    for (int index = 0; index < label.Length; index++)
+                    {
+                        if (!char.IsLetter(label[index]))
+                        {
+                            label = label.Substring(0, index);
+                            break;
+                        }
+                    }
+                }
+
+                yield return new ModelInput()
+                {
+                    ImagePath = file,
+                    Label = label
+                };
+            }
+        }
+    }
+
+    class ModelInput
+    {
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+    }
+
+    class ModelOutput
+    {
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+
+        public string PredictedLabel { get; set; }
+    }
+}
