846b09118f5af5208b4fa845d7dc82f2e4d325c4
Max Akbar <maximum2000@hotmail.com>
2019-10-27 19:06:40 -0700

    Added case for word embedding.

--- samples/csharp/getting-started/Clustering_NewsArticles/ClusteringNewsArticles.Train/Program.cs
+++ samples/csharp/getting-started/Clustering_NewsArticles/ClusteringNewsArticles.Train/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Common;
 using Microsoft.ML;
 using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Text;
+using Newtonsoft.Json.Schema;
 
 namespace ClusteringNewsArticles.Train
 {
@@ -10,23 +14,34 @@ namespace ClusteringNewsArticles.Train
     {
         private static void Main(string[] args)
         {
+            var textTransform = ValidateArgs(args);
+
             var assetsPath = Program.GetAbsolutePath("assets");
             var newsArticlesCsv = Path.Combine(assetsPath, "inputs", "newsArticles.csv");
             var modelPath = Path.Combine(assetsPath, "outputs", "newsArticlesClustering.zip");
+
             try
             {
-                var mlContext = new MLContext(new int?(1));
+                var mlContext = new MLContext(1);
                 var pivotDataView = mlContext.Data.LoadFromTextFile(newsArticlesCsv, new []
                 {
                     new TextLoader.Column("news_articles", DataKind.String, 0)
                 }, ',', true);
-                
-                var dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText("Features", "news_articles");
+                IEstimator<ITransformer> dataProcessPipeline;
 
-                //var wordEmbeddingEstimator = WordEmbeddingEstimator.PretrainedModelKind.SentimentSpecificWordEmbedding;
-                //var dataProcessPipeline = mlContext.Transforms.Text.NormalizeText("news_articles")
-                //    .Append(mlContext.Transforms.Text.TokenizeIntoWords("Tokens", "news_articles"))
-                //    .Append(mlContext.Transforms.Text.ApplyWordEmbedding("Features","Tokens", wordEmbeddingEstimator));
+                switch (textTransform)
+                {
+                    case "ApplyWordEmbedding":
+                        var wordEmbeddingEstimator = WordEmbeddingEstimator.PretrainedModelKind.SentimentSpecificWordEmbedding;
+                        dataProcessPipeline = mlContext.Transforms.Text.NormalizeText("news_articles")
+                            .Append(mlContext.Transforms.Text.TokenizeIntoWords("Tokens", "news_articles"))
+                            .Append(mlContext.Transforms.Text.ApplyWordEmbedding("Features", "Tokens", wordEmbeddingEstimator));
+                        break;
+                    default:
+                        dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText("Features", "news_articles");
+                        break;
+
+                }
 
                 ConsoleHelper.PeekDataViewInConsole(mlContext, pivotDataView, dataProcessPipeline, 10);
                 ConsoleHelper.PeekVectorColumnDataInConsole(mlContext, "Features", pivotDataView, dataProcessPipeline, 10);
@@ -51,13 +66,25 @@ namespace ClusteringNewsArticles.Train
             }
             catch (Exception ex)
             {
-                ConsoleHelper.ConsoleWriteException(new string[]
-                {
-                    ex.ToString()
-                });
+                ConsoleHelper.ConsoleWriteException(ex.ToString());
             }
 
             ConsoleHelper.ConsolePressAnyKey();
+        }
+
+        private static string ValidateArgs(IReadOnlyList<string> argument)
+        {
+            var argumentOptions = new[] {"ApplyWordEmbedding", "FeaturizeText" };
+            var message = "Parameter passed options are 'ApplyWordEmbedding and 'FeaturizeText' default will be used 'FeaturizeText'.";
+
+            if (!argument.Any() || !argumentOptions.ToList().Contains(argument[0]))
+            {
+                ConsoleHelper.ConsoleWriteWarning(message);
+
+                return argumentOptions[1];
+            }
+
+            return argument[0];
         }
 
         public static string GetAbsolutePath(string relativePath)
