3ed518c63faf65a13470f962559dde8a3bcadbfb
Luis Quintanilla <46974588+luisquintanilla@users.noreply.github.com>
2019-10-29 14:49:42 -0400

    Updated code to run on Azure (#725)

--- samples/csharp/end-to-end-apps/ScalableMLModelOnAzureFunction/SentimentAnalysisFunctionsApp/Startup.cs
+++ samples/csharp/end-to-end-apps/ScalableMLModelOnAzureFunction/SentimentAnalysisFunctionsApp/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.ML;
 using SentimentAnalysisFunctionsApp;
 using SentimentAnalysisFunctionsApp.DataModels;
@@ -8,10 +10,28 @@ namespace SentimentAnalysisFunctionsApp
 {
     public class Startup : FunctionsStartup
     {
+        private readonly string _environment;
+        private readonly string _modelPath;
+
+        public Startup()
+        {
+            _environment = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT");
+
+            if (_environment == "Development")
+            {
+                _modelPath = Path.Combine("MLModels", "sentiment_model.zip");
+            }
+            else
+            {
+                string deploymentPath = @"D:\home\site\wwwroot\";
+                _modelPath = Path.Combine(deploymentPath, "MLModels", "sentiment_model.zip");
+            }
+        }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddPredictionEnginePool<SentimentData, SentimentPrediction>()
-                .FromFile(modelName: "SentimentAnalysisModel", filePath:"MLModels/sentiment_model.zip", watchForChanges: true);
+                .FromFile(modelName: "SentimentAnalysisModel", filePath: _modelPath, watchForChanges: true);
         }
     }
 }
