cbacd6022ad3f129825f77dfdbbc981ab38e6fa7
Luis Quintanilla <46974588+luisquintanilla@users.noreply.github.com>
2019-10-28 20:25:50 -0400

    Model Builder Multiclass Classification sample for Docs tutorial (#697)
    * Initial commit
    
    * Initial Commit
    
    * Updated solution name and README
    
    * Removing irrelevant files
    
    * Renaming top-level folder
    
    * Working
    
    * Adding condensed dataset
    
    * Updated to use GH hosted dataset
    
    * Updated README and console output
    
    * Fixed project names in README
    
    * Updated data source
    
    * Back to working state with updates to use DB
    
    * Updated README to emphasize CSharp
    
    * Added C#
    
    * Updated based n feedback

---
+++ samples/modelbuilder/MulticlassClassification_RestaurantViolations/RestaurantViolations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RestaurantViolationsML.Model;
+
+namespace RestaurantViolations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create sample data
+            ModelInput input = new ModelInput
+            {
+                InspectionType = "Complaint",
+                ViolationDescription = "Inadequate sewage or wastewater disposal"
+            };
+
+            // Make prediction
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            // Print Prediction
+            Console.WriteLine($"Inspection type: {input.InspectionType}");
+            Console.WriteLine($"Violation description: {input.ViolationDescription}");
+            Console.WriteLine($"Predicted risk category: {result.Prediction}");
+            Console.ReadKey();
+        }
+    }
+}
