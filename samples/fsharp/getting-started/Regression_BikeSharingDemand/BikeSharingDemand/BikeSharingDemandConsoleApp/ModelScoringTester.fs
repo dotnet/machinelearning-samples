fa3f57a9fd957270000e39b49403ccbb909b1bf2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-12-27 11:21:11 +0100

    Cleanup ModelScoringTester.fs

--- samples/fsharp/getting-started/Regression_BikeSharingDemand/BikeSharingDemand/BikeSharingDemandConsoleApp/ModelScoringTester.fs
+++ samples/fsharp/getting-started/Regression_BikeSharingDemand/BikeSharingDemand/BikeSharingDemandConsoleApp/ModelScoringTester.fs
@@ -6,37 +6,38 @@ open DataStructures
 open Microsoft.ML
 open System.Globalization
 
+let parseSingle (s:string) = Single.Parse(s, CultureInfo.InvariantCulture)
+
 let readSampleDataFromCsvFile dataLocation numberOfRecordsToRead =
-    File.ReadLines(dataLocation)
+    File.ReadLines dataLocation
     |> Seq.skip 1
-    |> Seq.filter(fun x -> not(String.IsNullOrEmpty(x)))
-    |> Seq.map(fun x -> x.Split(','))
-    |> Seq.map(fun x -> {
-        Season = Single.Parse(x.[2], CultureInfo.InvariantCulture)
-        Year = Single.Parse(x.[3], CultureInfo.InvariantCulture)
-        Month = Single.Parse(x.[4], CultureInfo.InvariantCulture)
-        Hour = Single.Parse(x.[5], CultureInfo.InvariantCulture)
-        Holiday = Single.Parse(x.[6], CultureInfo.InvariantCulture)
-        Weekday = Single.Parse(x.[7], CultureInfo.InvariantCulture)
-        WorkingDay = Single.Parse(x.[8], CultureInfo.InvariantCulture)
-        Weather = Single.Parse(x.[9], CultureInfo.InvariantCulture)
-        Temperature = Single.Parse(x.[10], CultureInfo.InvariantCulture)
-        NormalizedTemperature = Single.Parse(x.[11], CultureInfo.InvariantCulture)
-        Humidity = Single.Parse(x.[12], CultureInfo.InvariantCulture)
-        Windspeed = Single.Parse(x.[13], CultureInfo.InvariantCulture)
-        Count = Single.Parse(x.[16], CultureInfo.InvariantCulture)
-    })
+    |> Seq.filter (fun x -> not (String.IsNullOrEmpty x))
+    |> Seq.map (fun x ->
+        let fields = x.Split(',')
+        { Season = parseSingle fields.[2]
+          Year = parseSingle fields.[3]
+          Month = parseSingle fields.[4]
+          Hour = parseSingle fields.[5]
+          Holiday = parseSingle fields.[6]
+          Weekday = parseSingle fields.[7]
+          WorkingDay = parseSingle fields.[8]
+          Weather = parseSingle fields.[9]
+          Temperature = parseSingle fields.[10]
+          NormalizedTemperature = parseSingle fields.[11]
+          Humidity = parseSingle fields.[12]
+          Windspeed = parseSingle fields.[13]
+          Count = parseSingle fields.[16] })
     |> Seq.take numberOfRecordsToRead
     |> Seq.toList
 
-
 let visualizeSomePredictions testDataLocation (predEngine : PredictionEngine<DemandObservation, DemandPrediction>) numberOfPredictions =
-    //Make a few prediction tests 
+    //Make a few prediction tests
     // Make the provided number of predictions and compare with observed data from the test dataset
 
     let testData = readSampleDataFromCsvFile testDataLocation numberOfPredictions
-    for i in 0 .. (numberOfPredictions - 1) do
+    for row in testData do
         //Score
-        let resultprediction = predEngine.Predict testData.[i]
-        Common.ConsoleHelper.printRegressionPredictionVersusObserved (float resultprediction.PredictedCount) (testData.[i].Count.ToString())
-    
+        let resultprediction = predEngine.Predict row
+        Common.ConsoleHelper.printRegressionPredictionVersusObserved
+            (float resultprediction.PredictedCount)
+            (row.Count.ToString())
