475dd4b82f1cc6820816c06d27b16ff3fa993ba4
J W <woodjon@gmail.com>
2020-09-23 07:50:33 -0400

    Initial add of project

---
+++ samples/csharp/getting-started/Ranking_AutoML/Ranking/Ranking/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML;
+using System;
+
+namespace Ranking
+{
+    class Program
+    {
+        const string TrainDatasetUrl = "https://aka.ms/mlnet-resources/benchmarks/MSLRWeb10KTrain720kRows.tsv";
+
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(seed: 0);
+
+            BuildTrainEvaluateAndSaveModel(mlContext);
+
+            TestSinglePrediction(mlContext);
+
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
+        }
+
+        private static void TestSinglePrediction(MLContext mlContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void BuildTrainEvaluateAndSaveModel(MLContext mlContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
