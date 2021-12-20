fa027d3d4938799fdfbf201e13e72db1d05e1a2e
Jon Wood <woodjon@gmail.com>
2021-12-20 08:39:27 -0500

    Update samples to .NET 6 (#932)
    * Update bike rental sample to .NET 6
    
    * Update object detection sample to .NET 6

--- samples/csharp/getting-started/Forecasting_BikeSharingDemand/BikeDemandForecasting/Program.cs
+++ samples/csharp/getting-started/Forecasting_BikeSharingDemand/BikeDemandForecasting/Program.cs
@@ -1,137 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.IO;
-using System.Linq;
-using Microsoft.ML;
+﻿using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Transforms.TimeSeries;
+using System.Data.SqlClient;
 
-namespace BikeDemandForecasting
+string rootDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../"));
+string dbFilePath = Path.Combine(rootDir, "Data", "DailyDemand.mdf");
+string modelPath = Path.Combine(rootDir, "MLModel.zip");
+var connectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={dbFilePath};Integrated Security=True;Connect Timeout=30;";
+
+MLContext mlContext = new MLContext();
+
+DatabaseLoader loader = mlContext.Data.CreateDatabaseLoader<ModelInput>();
+
+string query = "SELECT RentalDate, CAST(Year as REAL) as Year, CAST(TotalRentals as REAL) as TotalRentals FROM Rentals";
+
+DatabaseSource dbSource = new DatabaseSource(SqlClientFactory.Instance,
+                                connectionString,
+                                query);
+
+IDataView dataView = loader.Load(dbSource);
+
+IDataView firstYearData = mlContext.Data.FilterRowsByColumn(dataView, "Year", upperBound: 1);
+IDataView secondYearData = mlContext.Data.FilterRowsByColumn(dataView, "Year", lowerBound: 1);
+
+var forecastingPipeline = mlContext.Forecasting.ForecastBySsa(
+    outputColumnName: "ForecastedRentals",
+    inputColumnName: "TotalRentals",
+    windowSize: 7,
+    seriesLength: 30,
+    trainSize: 365,
+    horizon: 7,
+    confidenceLevel: 0.95f,
+    confidenceLowerBoundColumn: "LowerBoundRentals",
+    confidenceUpperBoundColumn: "UpperBoundRentals");
+
+SsaForecastingTransformer forecaster = forecastingPipeline.Fit(firstYearData);
+
+Evaluate(secondYearData, forecaster, mlContext);
+
+var forecastEngine = forecaster.CreateTimeSeriesEngine<ModelInput, ModelOutput>(mlContext);
+forecastEngine.CheckPoint(mlContext, modelPath);
+
+Forecast(secondYearData, 7, forecastEngine, mlContext);
+
+void Evaluate(IDataView testData, ITransformer model, MLContext mlContext)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string rootDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../"));
-            string dbFilePath = Path.Combine(rootDir, "Data", "DailyDemand.mdf");
-            string modelPath = Path.Combine(rootDir, "MLModel.zip");
-            var connectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={dbFilePath};Integrated Security=True;Connect Timeout=30;";
+    // Make predictions
+    IDataView predictions = model.Transform(testData);
 
-            MLContext mlContext = new MLContext();
+    // Actual values
+    IEnumerable<float> actual =
+        mlContext.Data.CreateEnumerable<ModelInput>(testData, true)
+            .Select(observed => observed.TotalRentals);
 
-            DatabaseLoader loader = mlContext.Data.CreateDatabaseLoader<ModelInput>();
+    // Predicted values
+    IEnumerable<float> forecast =
+        mlContext.Data.CreateEnumerable<ModelOutput>(predictions, true)
+            .Select(prediction => prediction.ForecastedRentals[0]);
 
-            string query = "SELECT RentalDate, CAST(Year as REAL) as Year, CAST(TotalRentals as REAL) as TotalRentals FROM Rentals";
+    // Calculate error (actual - forecast)
+    var metrics = actual.Zip(forecast, (actualValue, forecastValue) => actualValue - forecastValue);
 
-            DatabaseSource dbSource = new DatabaseSource(SqlClientFactory.Instance,
-                                            connectionString,
-                                            query);
+    // Get metric averages
+    var MAE = metrics.Average(error => Math.Abs(error)); // Mean Absolute Error
+    var RMSE = Math.Sqrt(metrics.Average(error => Math.Pow(error, 2))); // Root Mean Squared Error
 
-            IDataView dataView = loader.Load(dbSource);
+    // Output metrics
+    Console.WriteLine("Evaluation Metrics");
+    Console.WriteLine("---------------------");
+    Console.WriteLine($"Mean Absolute Error: {MAE:F3}");
+    Console.WriteLine($"Root Mean Squared Error: {RMSE:F3}\n");
+}
 
-            IDataView firstYearData = mlContext.Data.FilterRowsByColumn(dataView, "Year", upperBound: 1);
-            IDataView secondYearData = mlContext.Data.FilterRowsByColumn(dataView, "Year", lowerBound: 1);
+void Forecast(IDataView testData, int horizon, TimeSeriesPredictionEngine<ModelInput, ModelOutput> forecaster, MLContext mlContext)
+{
 
-            var forecastingPipeline = mlContext.Forecasting.ForecastBySsa(
-                outputColumnName: "ForecastedRentals",
-                inputColumnName: "TotalRentals",
-                windowSize: 7,
-                seriesLength: 30,
-                trainSize: 365,
-                horizon: 7,
-                confidenceLevel: 0.95f,
-                confidenceLowerBoundColumn: "LowerBoundRentals",
-                confidenceUpperBoundColumn: "UpperBoundRentals");
+    ModelOutput forecast = forecaster.Predict();
 
-            SsaForecastingTransformer forecaster = forecastingPipeline.Fit(firstYearData);
-
-            Evaluate(secondYearData, forecaster, mlContext);
-
-            var forecastEngine = forecaster.CreateTimeSeriesEngine<ModelInput, ModelOutput>(mlContext);
-            forecastEngine.CheckPoint(mlContext, modelPath);
-
-            Forecast(secondYearData, 7, forecastEngine, mlContext);
-
-            Console.ReadKey();
-        }
-
-        static void Evaluate(IDataView testData, ITransformer model, MLContext mlContext)
-        {
-            // Make predictions
-            IDataView predictions = model.Transform(testData);
-
-            // Actual values
-            IEnumerable<float> actual =
-                mlContext.Data.CreateEnumerable<ModelInput>(testData, true)
-                    .Select(observed => observed.TotalRentals);
-
-            // Predicted values
-            IEnumerable<float> forecast =
-                mlContext.Data.CreateEnumerable<ModelOutput>(predictions, true)
-                    .Select(prediction => prediction.ForecastedRentals[0]);
-
-            // Calculate error (actual - forecast)
-            var metrics = actual.Zip(forecast, (actualValue, forecastValue) => actualValue - forecastValue);
-
-            // Get metric averages
-            var MAE = metrics.Average(error => Math.Abs(error)); // Mean Absolute Error
-            var RMSE = Math.Sqrt(metrics.Average(error => Math.Pow(error, 2))); // Root Mean Squared Error
-
-            // Output metrics
-            Console.WriteLine("Evaluation Metrics");
-            Console.WriteLine("---------------------");
-            Console.WriteLine($"Mean Absolute Error: {MAE:F3}");
-            Console.WriteLine($"Root Mean Squared Error: {RMSE:F3}\n");
-        }
-
-        static void Forecast(IDataView testData, int horizon, TimeSeriesPredictionEngine<ModelInput, ModelOutput> forecaster, MLContext mlContext)
-        {
-
-            ModelOutput forecast = forecaster.Predict();
-
-            IEnumerable<string> forecastOutput =
-                mlContext.Data.CreateEnumerable<ModelInput>(testData, reuseRowObject: false)
-                    .Take(horizon)
-                    .Select((ModelInput rental, int index) =>
-                    {
-                        string rentalDate = rental.RentalDate.ToShortDateString();
-                        float actualRentals = rental.TotalRentals;
-                        float lowerEstimate = Math.Max(0, forecast.LowerBoundRentals[index]);
-                        float estimate = forecast.ForecastedRentals[index];
-                        float upperEstimate = forecast.UpperBoundRentals[index];
-                        return $"Date: {rentalDate}\n" +
-                        $"Actual Rentals: {actualRentals}\n" +
-                        $"Lower Estimate: {lowerEstimate}\n" +
-                        $"Forecast: {estimate}\n" +
-                        $"Upper Estimate: {upperEstimate}\n";
-                    });
-
-            // Output predictions
-            Console.WriteLine("Rental Forecast");
-            Console.WriteLine("---------------------");
-            foreach (var prediction in forecastOutput)
+    IEnumerable<string> forecastOutput =
+        mlContext.Data.CreateEnumerable<ModelInput>(testData, reuseRowObject: false)
+            .Take(horizon)
+            .Select((ModelInput rental, int index) =>
             {
-                Console.WriteLine(prediction);
-            }
-        }
-    }
+                string rentalDate = rental.RentalDate.ToShortDateString();
+                float actualRentals = rental.TotalRentals;
+                float lowerEstimate = Math.Max(0, forecast.LowerBoundRentals[index]);
+                float estimate = forecast.ForecastedRentals[index];
+                float upperEstimate = forecast.UpperBoundRentals[index];
+                return $"Date: {rentalDate}\n" +
+                $"Actual Rentals: {actualRentals}\n" +
+                $"Lower Estimate: {lowerEstimate}\n" +
+                $"Forecast: {estimate}\n" +
+                $"Upper Estimate: {upperEstimate}\n";
+            });
 
-    public class ModelInput
+    // Output predictions
+    Console.WriteLine("Rental Forecast");
+    Console.WriteLine("---------------------");
+    foreach (var prediction in forecastOutput)
     {
-        public DateTime RentalDate { get; set; }
-
-        public float Year { get; set; }
-
-        public float TotalRentals { get; set; }
+        Console.WriteLine(prediction);
     }
+}
 
-    public class ModelOutput
-    {
-        public float[] ForecastedRentals { get; set; }
+public class ModelInput
+{
+    public DateTime RentalDate { get; set; }
 
-        public float[] LowerBoundRentals { get; set; }
+    public float Year { get; set; }
 
-        public float[] UpperBoundRentals { get; set; }
-    }
+    public float TotalRentals { get; set; }
+}
+
+public class ModelOutput
+{
+    public float[] ForecastedRentals { get; set; }
+
+    public float[] LowerBoundRentals { get; set; }
+
+    public float[] UpperBoundRentals { get; set; }
 }
