204836f21ef303a4e6a574020361aea78d74db30
Patrice DARGENTON <patrice.dargenton@free.fr>
2022-03-20 08:11:56 +0100

    Big files excluded from: DLImgClassificationTF samples (C# and F#)

--- samples/csharp/getting-started/DeepLearning_ImageClassification_TensorFlow/ImageClassification/Program.cs
+++ samples/csharp/getting-started/DeepLearning_ImageClassification_TensorFlow/ImageClassification/Program.cs
@@ -1,5 +1,7 @@
-﻿using ImageClassification.ModelScorer;
+﻿using Common;
+using ImageClassification.ModelScorer;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 
@@ -12,10 +14,28 @@ namespace ImageClassification
             string assetsRelativePath = @"../../../assets";
             string assetsPath = GetAbsolutePath(assetsRelativePath);
 
+            // https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip = inception-v1.zip
+            const string inceptionFile = "inception5h"; //"inception-v1";
+            const string inceptionGraph = "tensorflow_inception_graph.pb";
+
+            const string inceptionGraphZip = inceptionFile + ".zip";
+            const string inceptionGraphUrl =
+                "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip"; // v1
+
             var tagsTsv = Path.Combine(assetsPath, "inputs", "images", "tags.tsv");
             var imagesFolder = Path.Combine(assetsPath, "inputs", "images");
-            var inceptionPb = Path.Combine(assetsPath, "inputs", "inception", "tensorflow_inception_graph.pb");
-            var labelsTxt = Path.Combine(assetsPath, "inputs", "inception", "imagenet_comp_graph_label_strings.txt");
+            var inceptionPb = Path.Combine(assetsPath, "inputs", "inception", inceptionGraph);
+            var labelsTxt = Path.Combine(assetsPath, "inputs", "inception", 
+                "imagenet_comp_graph_label_strings.txt");
+
+            var inceptionFolder = Path.Combine(assetsPath, "inputs", "tensorflow-pretrained-models");
+            var commonGraphsRelativePath = @"../../../../../../../../graphs";
+            var commonGraphsPath = GetAbsolutePath(commonGraphsRelativePath);
+
+            var fullIncepionFolderPath = Path.Combine(
+                inceptionFolder, Path.GetFileNameWithoutExtension(inceptionGraphZip));
+            List<string> destGraphFile = new List<string>() { inceptionPb };
+            Web.DownloadBigFile(fullIncepionFolderPath, inceptionGraphUrl, inceptionGraphZip, commonGraphsPath, destGraphFile);
 
             try
             {
