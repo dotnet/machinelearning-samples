7838714583f06d2272de4f0264434fa1bc0bbb24
Jon Wood <woodjon@gmail.com>
2022-04-17 10:03:24 +0000

    Adding weather recognition sample

---
+++ samples/csharp/end-to-end-apps/WeatherRecognition_ONNX/WeatherRecognition/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Transforms.Image;
+using System.Drawing;
+using WeatherRecognition;
+
+var context = new MLContext();
+
+var emptyData = new List<WeatherRecognitionInput>();
+
+var data = context.Data.LoadFromEnumerable(emptyData);
+
+var pipeline = context.Transforms.ResizeImages(resizing: ImageResizingEstimator.ResizingKind.Fill, outputColumnName: "data", imageWidth: 300, imageHeight: 300, inputColumnName: nameof(WeatherRecognitionInput.Image))
+                .Append(context.Transforms.ExtractPixels(outputColumnName: "data"))
+                .Append(context.Transforms.ApplyOnnxModel(modelFile: "./model/model.onnx", outputColumnName: "model_output", inputColumnName: "data"));
+
+var model = pipeline.Fit(data);
+
+var predictionEngine = context.Model.CreatePredictionEngine<WeatherRecognitionInput, WeatherRecognitionPrediction>(model);
+
+var labels = File.ReadAllLines("./model/labels.txt");
+
+var testFiles = Directory.GetFiles("./test");
+
+Bitmap testImage;
+
+foreach (var image in testFiles)
+{
+    using (var stream = new FileStream(image, FileMode.Open))
+    {
+        testImage = (Bitmap)Image.FromStream(stream);
+    }
+
+    var prediction = predictionEngine.Predict(new WeatherRecognitionInput { Image = testImage });
+
+    var maxValue = prediction.PredictedLabels.Max();
+    var maxIndex = prediction.PredictedLabels.ToList().IndexOf(maxValue);
+
+    var predictedLabel = labels[maxIndex];
+
+    Console.WriteLine($"Prediction for file {image}: {predictedLabel}");
+}
---
+++ samples/csharp/end-to-end-apps/WeatherRecognition_ONNX/WeatherRecognition/WeatherRecognitionPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace WeatherRecognition
+{
+    public class WeatherRecognitionPrediction
+    {
+        [ColumnName("model_output")]
+        public float[] PredictedLabels { get; set; }
+    }
+}
